9f2e568ffbb06d7c20254f145eb2634fb9088196
Florian Zand <florianzand@me.com>
2023-06-01 02:23:42 +0200

    Update KeyValueObserver.swift

--- Sources/FZSwiftUtils/Extensions/NSObject/KeyValueObserver.swift
+++ Sources/FZSwiftUtils/Extensions/NSObject/KeyValueObserver.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 
-class KeyValueObserver<Object> where Object: NSObject {
+public class KeyValueObserver<Object> where Object: NSObject {
     internal var observers: [String: NSKeyValueObservation] = [:]
+    internal var handlers: [String: ((Object, _ oldValue: Any, _ newValue: Any)->())] = [:]
+
     public fileprivate(set) weak var object: Object?
     
     public init(_ object: Object) {
@@ -43,10 +45,27 @@ class KeyValueObserver<Object> where Object: NSObject {
         }
     }
     
-    public func add<Value>(_ keyPath: KeyPath<Object, Value>, handler: @escaping ((Object, _ oldValue: Value, _ newValue: Value)->())) {
-        guard let name = keyPath._kvcKeyPathString else { return }
+    public func add<Value>(_ keypath: KeyPath<Object, Value>, handler: @escaping ((Object, _ oldValue: Value, _ newValue: Value)->())) {
+        guard let name = keypath._kvcKeyPathString else { return }
         if (observers[name] == nil) {
-            observers[name] = object?.observeChange(keyPath, handler: handler)
+            observers[name] = object?.observeChange(keypath, handler: handler)
         }
     }
+    
+    subscript<Value>(keypath: KeyPath<Object, Value>) -> ((Object, _ oldValue: Value, _ newValue: Value)->())? {
+        get {
+            guard let name = keypath._kvcKeyPathString else { return nil }
+            guard let handler = handlers[name] else { return nil }
+            return handler as ((Object, _ oldValue: Value, _ newValue: Value)->())
+        }
+        set {
+            guard let name = keypath._kvcKeyPathString else { return }
+            if (observers[name] != nil) {
+                self.remove(keypath)
+            }
+            guard let newValue = newValue else { return }
+            self.add(keypath, handler: newValue)
+        }
+    }
+    
 }
