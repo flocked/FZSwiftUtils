a053b97a1c4acb3c13468f6f1c6caaa203ac9bb2
Florian Zand <florianzand@me.com>
2023-06-01 02:06:20 +0200

    Update NSMetadata+.swift

--- Sources/FZSwiftUtils/Extensions/URL/NSMetadata+.swift
+++ Sources/FZSwiftUtils/Extensions/URL/NSMetadata+.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-extension NSMetadataItem {
-    func value<T>(_ attribute: String, type _: T.Type) -> T? {
-        return value(forAttribute: attribute) as? T
+public extension NSMetadataItem {
+    func value<T>(for attribute: String) -> T? {
+        return self.value(forAttribute: attribute) as? T
     }
 }
 
 public extension NSMetadataQuery {
     func values(of attributes: [String], forResultsAt index: Int) -> [String: Any] {
         var values = [String: Any]()
-        attributes.forEach { values[$0] = value(ofAttribute: $0, forResultAt: index) }
+        attributes.forEach({ values[$0] = value(ofAttribute: $0, forResultAt: index) })
         return values
     }
 }
