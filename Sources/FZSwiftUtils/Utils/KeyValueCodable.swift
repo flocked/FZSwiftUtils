b8ca312eaaa19950c0320ac90a6448dc362db931
Florian Zand <florianzand@me.com>
2023-07-24 05:43:03 +0200

    Update KeyValueCodable.swift

--- Sources/FZSwiftUtils/Utils/KeyValueCodable.swift
+++ Sources/FZSwiftUtils/Utils/KeyValueCodable.swift
@@ -10,12 +10,18 @@ import Foundation
 public protocol KeyValueCodable {
     /// Returns the value for the property identified by a given key.
     func value(for key: String) -> Any?
-    
-    /// Sets properties of the receiver with values from a given dictionary, using its keys to identify the properties.
-    func setValuesForKeys(_ keyedValues: [String : Any])
-    
+        
     /// Sets the property of the receiver specified by a given key to a given value.
     func setValue(_ value: Any?, for key: String)
     
-    func callSelector(_ selector: Selector, with values: [Any?])
+}
+
+public extension KeyValueCodable {
+    func value(for key: String) -> Any? {
+        return nil
+    }
+    
+    func setValue(_ value: Any?, for key: String) {
+        
+    }
 }
