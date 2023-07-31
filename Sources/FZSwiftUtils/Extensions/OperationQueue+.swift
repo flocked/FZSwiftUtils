282b3da13e5d20c733afe281ae5cc4d17fb1171d
Florian Zand <florianzand@me.com>
2023-07-31 06:39:45 +0200

    Create OperationQueue+.swift

---
+++ Sources/FZSwiftUtils/Extensions/OperationQueue+.swift
@@ -0,0 +1,21 @@
+//
+//  OperationQueue+.swift
+//  
+//
+//  Created by Florian Zand on 31.07.23.
+//
+
+import Foundation
+
+public extension OperationQueue {
+    /**
+     Initalizes an operation queue with the specified maximum number of queued operations that can run at the same time.
+     
+     - Parameters maxConcurrentOperationCount: The maximum number of queued operations that can run at the same time.
+     - Returns: A new `OperationQueue` object.
+     */
+    convenience init(maxConcurrentOperationCount: Int) {
+        self.init()
+        self.maxConcurrentOperationCount = maxConcurrentOperationCount
+    }
+}
