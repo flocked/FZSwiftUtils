c41dc245f9661ce4c3d187ed4e4a48f9efcd1221
Florian Zand <florianzand@me.com>
2023-07-31 06:22:45 +0200

    Update Number+String.swift

--- Sources/FZSwiftUtils/Extensions/Math & Geometry/Number+String.swift
+++ Sources/FZSwiftUtils/Extensions/Math & Geometry/Number+String.swift
@@ -8,28 +8,33 @@
 import Foundation
 
 public extension IntegerLiteralType {
+    /// The string value.
     var string: String {
         String(self)
     }
 }
 
 public extension FloatLiteralType {
+    /// The string value.
     var string: String {
         String(self)
     }
 }
 
 public extension CGFloat {
+    /// The string value.
     var string: String {
         String(Float(self))
     }
 }
 
 public extension NSNumber {
+    /// The integer string value.
     var intString: String {
         String(Int(truncating: self))
     }
     
+    /// The string value.
     var string: String {
         String(Float(truncating: self))
     }
