b64a6ee25e3a1d93532ca7d0cb209f481c0d60fe
Florian Zand <florianzand@me.com>
2023-08-25 02:40:23 +0200

    Update CGRect+.swift

--- Sources/FZSwiftUtils/Extensions/Math & Geometry/CGRect+.swift
+++ Sources/FZSwiftUtils/Extensions/Math & Geometry/CGRect+.swift
@@ -59,81 +59,9 @@ public extension CGRect {
             height: size.height.scaledIntegral
         )
     }
-
-    /**
-     The center point of the rect.
-     The getter returns a point representing the center point, while the setter sets the origin based on the specified center point.
-     */
-    var center: CGPoint {
-        get { return CGPoint(x: centerX, y: centerY) }
-        set { centerX = newValue.x; centerY = newValue.y }
-    }
-
-    /**
-     The bottom-left point of the rect.
-     The getter returns a point representing the bottom-left point, while the setter sets the origin based on the specified bottom-left point.
-     */
-    var bottomLeft: CGPoint {
-        get { return CGPoint(x: minX, y: minY) }
-        set { origin = newValue }
-    }
-
-    /**
-     The bottom-right point of the rect.
-     The getter returns a point representing the bottom-right point.
-     */
-    var bottomRight: CGPoint { return CGPoint(x: maxX, y: minY) }
-
-    /**
-     The top-left point of the rect.
-     The getter returns a point representing the top-left point, while the setter adjusts the origin and size to maintain the top-left position.
-     */
-    var topLeft: CGPoint {
-        get { return CGPoint(x: minX, y: maxY) }
-        set { origin = CGPoint(x: newValue.x, y: origin.y)
-            let width = origin.y - newValue.y
-            if width < 0 {
-                origin = CGPoint(x: origin.x, y: origin.y - width)
-            }
-            size.width = origin.y + width
-        }
-    }
-
-    /**
-     The top-right point of the rect.
-     The getter returns a point representing the top-right point, while the setter adjusts the origin and size to maintain the top-right position.
-     */
-    var topRight: CGPoint {
-        get { return CGPoint(x: maxX, y: maxY) }
-        set { origin = CGPoint(x: newValue.x, y: origin.y)
-            let width = origin.y - newValue.y
-            let height = origin.x - newValue.x
-            if width < 0 {
-                origin = CGPoint(x: origin.x, y: origin.y - width)
-            }
-            size.width = origin.y + width
-
-            if height < 0 {
-                origin = CGPoint(x: origin.x - height, y: origin.y)
-            }
-            size.height = origin.x + height
-        }
-    }
-
-    private var centerX: CGFloat {
-        get { return midX }
-        set { origin.x = newValue - width * 0.5 }
-    }
-
-    private var centerY: CGFloat {
-        get { return midY }
-        set { origin.y = newValue - height * 0.5 }
-    }
-
-    /**
-     The x-coordinate of the origin of the rect.
-     The getter returns the x-coordinate of the origin, while the setter updates the x-coordinate of the origin.
-     */
+    
+    
+    /// The x-coordinate of the origin of the rectangle.
     var x: CGFloat {
         get { origin.x }
         set {
@@ -143,10 +71,7 @@ public extension CGRect {
         }
     }
 
-    /**
-     The y-coordinate of the origin of the rect.
-     The getter returns the y-coordinate of the origin, while the setter updates the y-coordinate of the origin.
-     */
+    /// The y-coordinate of the origin of the rectangle.
     var y: CGFloat {
         get { origin.y }
         set {
@@ -155,24 +80,108 @@ public extension CGRect {
             self.origin = origin
         }
     }
+    
+    /// The left edge of the rectangle.
+    var left: CGFloat {
+        get {return origin.x}
+        set {origin.x = newValue}
+    }
+    /// The right edge of the rectangle.
+    var right: CGFloat {
+        get {return x + width}
+        set {x = newValue - width}
+    }
 
-    /*
-     var width: CGFloat {
-         get { self.size.width }
-         set {
-             var size = self.size
-             size.width = newValue
-             self.size = size }
-     }
+    #if os(iOS)
+    /// The top edge of the rectangle.
+    var top: CGFloat {
+        get {return y}
+        set {y = newValue}
+    }
+    /// The bottom edge of the rectangle.
+    var bottom: CGFloat {
+        get {return y + height}
+        set {y = newValue - height}
+    }
+    #else
+    /// The top edge of the rectangle.
+    var top: CGFloat {
+        get {return y + height}
+        set {y = newValue - height}
+    }
+    /// The bottom edge of the rectangle.
+    var bottom: CGFloat {
+        get {return y}
+        set {y = newValue}
+    }
+    #endif
 
-      var height: CGFloat {
-         get { self.size.height }
-         set {
-             var size = self.size
-             size.height = newValue
-             self.size = size }
-     }
-     */
+    /// The top-left point of the rectangle.
+    var topLeft: CGPoint {
+        get {return CGPoint(x: left, y: top)}
+        set {left = newValue.x; top = newValue.y}
+    }
+    
+    /// The top-center point of the rectangle.
+    var topCenter: CGPoint {
+        get {return CGPoint(x: centerX, y: top)}
+        set {centerX = newValue.x; top = newValue.y}
+    }
+    
+
+    /// The top-right point of the rectangle.
+    var topRight: CGPoint {
+        get {return CGPoint(x: right, y: top)}
+        set {right = newValue.x; top = newValue.y}
+    }
+    
+    /// The center-left point of the rectangle.
+    var centerLeft: CGPoint {
+        get {return CGPoint(x: left, y: centerY)}
+        set {left = newValue.x; centerY = newValue.y}
+    }
+    
+    /// The center point of the rectangle.
+    var center: CGPoint {
+        get { return CGPoint(x: centerX, y: centerY) }
+        set { centerX = newValue.x; centerY = newValue.y }
+    }
+    
+    /// The center-right point of the rectangle.
+    var centerRight: CGPoint {
+        get {return CGPoint(x: right, y: centerY)}
+        set {right = newValue.x; centerY = newValue.y}
+    }
+    
+    /// The bottom-left point of the rectangle.
+    var bottomLeft: CGPoint {
+        get {return CGPoint(x: left, y: bottom)}
+        set {left = newValue.x; bottom = newValue.y}
+    }
+    
+    /// The bottom-center point of the rectangle.
+    var bottomCenter: CGPoint {
+        get {return CGPoint(x: centerX, y: bottom)}
+        set {centerX = newValue.x; bottom = newValue.y}
+    }
+    
+    /// The bottom-right point of the rectangle.
+    var bottomRight: CGPoint {
+        get {return CGPoint(x: right, y: bottom)}
+        set {right = newValue.x; bottom = newValue.y}
+    }
+    
+    /// The horizontal center of the rectangle.
+    var centerX: CGFloat {
+        get { return midX }
+        set { origin.x = newValue - width * 0.5 }
+    }
+
+    /// The vertical center of the rectangle.
+    var centerY: CGFloat {
+        get { return midY }
+        set { origin.y = newValue - height * 0.5 }
+    }
 
     /// The edge direction used for expanding a rect.
     enum ExpandEdge {
@@ -353,4 +362,9 @@ extension Collection where Element == CGRect {
         }
         return unionRect
     }
+}
+
+extension CGRect {
+
+
 }
