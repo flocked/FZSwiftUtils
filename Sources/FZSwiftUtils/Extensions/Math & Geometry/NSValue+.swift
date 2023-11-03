d67be4e4334e739ee2ed9cd6d8a4ba5452ba42bf
Florian Zand <florianzand@me.com>
2023-11-03 09:40:37 +0100

    updated

--- Sources/FZSwiftUtils/Extensions/Math & Geometry/NSValue+.swift
+++ Sources/FZSwiftUtils/Extensions/Math & Geometry/NSValue+.swift
@@ -10,25 +10,23 @@ import Foundation
 import QuartzCore
 #endif
 
-extension NSValue {
-    /// Creates a new value object containing the specified directional edge insets structure.
-    public convenience init(nsDirectionalEdgeInsets: NSDirectionalEdgeInsets) {
-        var insets = nsDirectionalEdgeInsets
-        self.init(bytes: &insets, objCType: _getObjCTypeEncoding(NSDirectionalEdgeInsets.self))
-    }
-    
-    /// Returns the directional edge insets structure representation of the value.
-    public var nsDirectionalEdgeInsetsValue: NSDirectionalEdgeInsets {
-        var insets = NSDirectionalEdgeInsets()
-        self.getValue(&insets)
-        return insets
-    }
-}
-
 #if os(macOS)
 import AppKit
 
 extension NSValue {
+    /// Creates a new value object containing the specified directional edge insets structure.
+    public convenience init(directionalEdgeInsets: NSDirectionalEdgeInsets) {
+        var insets = directionalEdgeInsets
+        self.init(bytes: &insets, objCType: _getObjCTypeEncoding(NSDirectionalEdgeInsets.self))
+    }
+    
+    /// Returns the directional edge insets structure representation of the value.
+    public var directionalEdgeInsetsValue: NSDirectionalEdgeInsets {
+        var insets = NSDirectionalEdgeInsets()
+        self.getValue(&insets)
+        return insets
+    }
+    
     /// Creates a new value object containing the specified CoreGraphics affine transform structure.
     public convenience init(cgAffineTransform: CGAffineTransform) {
         var transform = cgAffineTransform
@@ -40,19 +38,6 @@ extension NSValue {
         var transform = CGAffineTransform.identity
         self.getValue(&transform)
         return transform
-    }
-    
-    /// Creates a new value object containing the specified AppKit edge insets structure.
-    public convenience init(nsEdgeInsets: NSEdgeInsets) {
-        var nsEdgeInsets = nsEdgeInsets
-        self.init(bytes: &nsEdgeInsets, objCType: _getObjCTypeEncoding(NSEdgeInsets.self))
-    }
-    
-    /// Returns the AppKit edge insets structure representation of the value.
-    public var nsEdgeInsetsValue: NSEdgeInsets {
-        var edgeInsets = NSEdgeInsets()
-        self.getValue(&edgeInsets)
-        return edgeInsets
     }
 }
 #endif
