c9cf7ce31707c06077cbee06ada14402b6ea6d4b
Florian Zand <florianzand@me.com>
2023-11-17 12:52:39 +0100

    Update Collection+Operator.swift

--- Sources/FZSwiftUtils/Extensions/Collections/Collection+Operator.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/Collection+Operator.swift
@@ -5,6 +5,7 @@
 //  Created by Florian Zand on 23.08.22.
 //
 
+/*
 import Foundation
 
 public extension RangeReplaceableCollection where Element: Equatable {
@@ -43,3 +44,4 @@ public extension RangeReplaceableCollection {
         lhs.append(rhs)
     }
 }
+*/
