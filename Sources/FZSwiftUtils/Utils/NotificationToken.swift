bff7299b2f5611e5f8bba0744804c5c3c0806c13
Florian Zand <florianzand@me.com>
2023-12-21 10:21:00 +0100

    Update NotificationToken.swift

--- Sources/FZSwiftUtils/Utils/NotificationToken.swift
+++ Sources/FZSwiftUtils/Utils/NotificationToken.swift
@@ -7,13 +7,17 @@
 
 import Foundation
 
-/// A token representing an observer for notifications.
+/**
+ A token representing an observer for notifications.
+ 
+ The notification is observed until you deallocate the token.
+ */
 public class NotificationToken: NSObject {
     /// The `NotificationCenter` instance associated with the observer.
     let notificationCenter: NotificationCenter
     
     /// The token that identifies the observer.
-    internal let token: Any
+    let token: Any
 
     /**
       Initializes a new `NotificationToken` instance with the specified `NotificationCenter` and token.
@@ -43,17 +47,17 @@ public extension NotificationCenter {
      
      - Parameters:
        - name: The name of the notification to observe. Pass `nil` to receive notifications for all names.
-       - obj: The object to observe. Pass `nil` to receive notifications from any object.
-       - queue: The operation queue on which to execute the block. Pass `nil` to use the default operation queue.
+       - object: The object to observe. Pass `nil` to receive notifications from any object.
+       - queue: The operation queue on which to execute the block. The default value is `nil` which uses the default queue.
        - block: The block to execute when the notification is received. The block takes a single parameter of type `Notification`.
      
      - Returns: A `NotificationToken` that represents the observer. You can use this token to remove the observer later.
      */
-    func observe(name: NSNotification.Name?, object obj: Any?,
+    func observe(_ name: NSNotification.Name?, object: Any?,
                  queue: OperationQueue? = nil, using block: @escaping (Notification) -> Void)
         -> NotificationToken
     {
-        let token = addObserver(forName: name, object: obj, queue: queue, using: block)
+        let token = addObserver(forName: name, object: object, queue: queue, using: block)
         return NotificationToken(notificationCenter: self, token: token)
     }
 }
