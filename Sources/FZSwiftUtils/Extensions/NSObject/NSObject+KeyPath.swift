2afcc5d5d906f34063db84bf95e926b63cc65c7f
Florian Zand <florianzand@me.com>
2024-01-06 12:57:10 +0100

    Update NSObject+KeyPath.swift

--- Sources/FZSwiftUtils/Extensions/NSObject/NSObject+KeyPath.swift
+++ Sources/FZSwiftUtils/Extensions/NSObject/NSObject+KeyPath.swift
@@ -9,7 +9,11 @@ import Foundation
 import AppKit
 
 extension NSObjectProtocol where Self: NSObject {
-    /// Returns the value for the derived property identified by a given key path.
+    /**
+     Returns the value for the derived property identified by a given key path.
+     
+     - Parameter keyPath: The keypath of the property.
+     */
     public func value<Value>(forKeyPath keyPath: KeyPath<Self, Value>) -> Value? {
         if let value = value(forKeyPath: keyPath.stringValue) {
             return value as? Value
@@ -17,7 +21,11 @@ extension NSObjectProtocol where Self: NSObject {
         return nil
     }
     
-    /// Returns the value for the derived property identified by a given key path.
+    /**
+     Returns the value for the derived property identified by a given key path.
+     
+     - Parameter keyPath: The keypath of the property.
+     */
     public func value<Value>(forKeyPath keyPath: KeyPath<Self, Value?>) -> Value? {
         if let value = value(forKeyPath: keyPath.stringValue) {
             return value as? Value
@@ -25,12 +33,24 @@ extension NSObjectProtocol where Self: NSObject {
         return nil
     }
     
-    /// Sets the value for the property identified by a given key path to a given value.
+    /**
+     Sets the value for the property identified by a given key path to a given value.
+     
+     - Parameters:
+        - value: The value of the property.
+        - keyPath: The keypath of the property.
+     */
     public func setValue<Value>(_ value: Value, forKeyPath keyPath: KeyPath<Self, Value>){
         self.setValue(value, forKeyPath: keyPath.stringValue)
     }
     
-    /// Sets the value for the property identified by a given key path to a given value.
+    /**
+     Sets the value for the property identified by a given key path to a given value.
+     
+     - Parameters:
+        - value: The value of the property.
+        - keyPath: The keypath of the property.
+     */
     public  func setValue<Value>(_ value: Value?, forKeyPath keyPath: KeyPath<Self, Value?>){
         self.setValue(value, forKeyPath: keyPath.stringValue)
     }
