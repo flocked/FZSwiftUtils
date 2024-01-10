fc10eab64c2d42656014b5a0ed2cd2e3b35c7613
Florian Zand <florianzand@me.com>
2024-01-10 02:42:35 +0100

    Update IndexPath+.swift

--- Sources/FZSwiftUtils/Extensions/Collections/IndexPath+.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/IndexPath+.swift
@@ -11,15 +11,23 @@ extension IndexPath {
     /// An index path with a `item` and `section` value of `0`.
     public static var zero: IndexPath { IndexPath(item: 0, section: 0) }
 
+    #if os(macOS) || os(iOS) || os(tvOS) || os(visionOS)
     /// `NSIndexPath` representation of the value.
     public var nsIndexPath: NSIndexPath {
+        #if os(macOS)
         NSIndexPath(forItem: item, inSection: section)
+        #else
+        NSIndexPath(item: item, section: section)
+        #endif
     }
+    #endif
 }
 
+#if os(macOS) || os(iOS) || os(tvOS) || os(visionOS)
 extension NSIndexPath {
     /// `IndexPath` representation of the value.
     public var indexPath: IndexPath {
         IndexPath(item: item, section: section)
     }
 }
+#endif
