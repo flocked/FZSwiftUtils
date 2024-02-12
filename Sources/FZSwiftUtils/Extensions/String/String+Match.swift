5ea65e53b3eba7ff799a3627253a61f560947678
Florian Zand <florianzand@me.com>
2024-02-12 14:15:35 +0100

    Update String+Match.swift

--- Sources/FZSwiftUtils/Extensions/String/String+Match.swift
+++ Sources/FZSwiftUtils/Extensions/String/String+Match.swift
@@ -90,6 +90,14 @@ public extension String {
     func matches(regex: String) -> [StringMatch] {
         let string = self
         let regex = try? NSRegularExpression(pattern: regex, options: [])
+        return regex?.matches(in: string, range: NSRange(string.startIndex..., in: string)).compactMap { 
+            
+            StringMatch($0, source: string) } ?? []
+    }
+    
+    func matchesAlt(regex: String) -> [StringMatch] {
+        let string = self
+        let regex = try? NSRegularExpression(pattern: regex, options: [])
         return regex?.matches(in: string, range: NSRange(string.startIndex..., in: string)).compactMap { StringMatch($0, source: string) } ?? []
     }
 
