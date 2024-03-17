0b3074b6d2343e07ed828be335a1efd3fe010126
Florian Zand <florianzand@me.com>
2024-03-17 22:13:39 +0100

    updated

--- Sources/FZSwiftUtils/Extensions/NSKeyedUnarchiver+.swift
+++ Sources/FZSwiftUtils/Extensions/NSKeyedUnarchiver+.swift
@@ -30,3 +30,29 @@ extension NSKeyedUnarchiver {
         case failedDecoding
     }
 }
+
+/*
+ extension NSKeyedArchiver {
+     static func archiver(forWritingWith data: NSMutableData) -> NSKeyedArchiver? {
+         if let allocatedObject = NSClassFromString("NSKeyedArchiver")?.alloc() as? NSObject {
+             let selector: Selector = NSSelectorFromString("initForWritingWithMutableData:")
+             let methodIMP: IMP! = allocatedObject.method(for: selector)
+             let objectAfterInit = unsafeBitCast(methodIMP,to:(@convention(c)(AnyObject?,Selector,NSMutableData)->NSObject).self)(allocatedObject,selector, data)
+             return objectAfterInit as? NSKeyedArchiver
+         }
+         return nil
+     }
+ }
+
+ extension NSKeyedUnarchiver {
+     static func unarchiver(forReadingWith data: NSData) -> NSKeyedUnarchiver? {
+         if let allocatedObject = NSClassFromString("NSKeyedUnarchiver")?.alloc() as? NSObject {
+             let selector: Selector = NSSelectorFromString("initForReadingWithData:")
+             let methodIMP: IMP! = allocatedObject.method(for: selector)
+             let objectAfterInit = unsafeBitCast(methodIMP,to:(@convention(c)(AnyObject?,Selector, NSData)->NSObject).self)(allocatedObject,selector, data)
+             return objectAfterInit as? NSKeyedUnarchiver
+         }
+         return nil
+     }
+ }
+ */
