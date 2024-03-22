463c183f3e425842e6d208fdc94c78e3e25be522
Florian Zand <florianzand@me.com>
2024-03-22 15:14:03 +0100

    Update Logger.swift

--- Sources/FZSwiftUtils/Utils/Logger.swift
+++ Sources/FZSwiftUtils/Utils/Logger.swift
@@ -9,18 +9,30 @@ import Foundation
 
 /// Logging utility that prints textual representations of objects.
 public struct Logger {
-    /// The activation state of the logger.
+    
+    /// The state of the logger.
     public enum State: Int {
-        /// The logger is active.
+        /// Active.
         case active
-        /// The logger is active when debugging.
-        case activeIfDebugging
-        /// The logger is inactive.
+        /// Active when debugging.
+        case activeWhenDebugging
+        /// Inactive.
         case inactive
     }
     
-    /// The activation state.
-    public static var state: State = .activeIfDebugging
+    /// The output format of the logger.
+    public enum OutputFormat: Int {
+        /// Normal.
+        case normal
+        /// Debug.
+        case debug
+    }
+    
+    /// The state of the logger.
+    public static var state: State = .activeWhenDebugging
+    
+    /// The output format of the logger.
+    public static var outputFormat: OutputFormat = .debug
     
     /**
      Writes the textual representations of the given items into the standard output.
@@ -32,11 +44,19 @@ public struct Logger {
      */
     public static func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
         guard state != .inactive else { return }
-        if state == .active {
-            Swift.print(items, separator: separator, terminator: terminator)
-        } else {
-            Swift.debugPrint(items, separator: separator, terminator: terminator)
+        func print() {
+            if outputFormat == .normal {
+                Swift.print(items, separator: separator, terminator: terminator)
+            } else {
+                Swift.debugPrint(items, separator: separator, terminator: terminator)
+            }
         }
+        #if DEBUG
+        print()
+        #else
+        guard state == .active else { return }
+        print()
+        #endif
     }
     
     /**
@@ -51,10 +71,18 @@ public struct Logger {
     public static func print<Target>(_ items: Any..., separator: String = " ", terminator: String = "\n", to output: inout Target
     ) where Target : TextOutputStream {
         guard state != .inactive else { return }
-        if state == .active {
-            Swift.print(items, separator: separator, terminator: terminator, to: &output)
-        } else {
-            Swift.debugPrint(items, separator: separator, terminator: terminator, to: &output)
+        func print() {
+            if outputFormat == .normal {
+                Swift.print(items, separator: separator, terminator: terminator, to: &output)
+            } else {
+                Swift.debugPrint(items, separator: separator, terminator: terminator, to: &output)
+            }
         }
+        #if DEBUG
+        print()
+        #else
+        guard state == .active else { return }
+        print()
+        #endif
     }
 }
