f5c0b9cc12fce90d6e57b84825bf78207d73813c
Florian Zand <florianzand@me.com>
2024-04-07 02:09:46 +0200

    Update MutableProgress.swift

--- Sources/FZSwiftUtils/Utils/MutableProgress.swift
+++ Sources/FZSwiftUtils/Utils/MutableProgress.swift
@@ -28,12 +28,12 @@ open class MutableProgress: Progress {
     
     /// All the current unfinished children progresses.
     open var unfinishedChildren: [Progress] {
-        observedChildren.keys.filter({!$0.isFinished})
+        observedChildren.keys.filter({$0.fractionCompleted < 100.0 && !$0.isCancelled})
     }
     
     /// All the current unfinished children progresses.
     open var finishedChildren: [Progress] {
-        observedChildren.keys.filter({$0.isFinished})
+        observedChildren.keys.filter({$0.fractionCompleted == 100.0})
     }
     
     /// The progress of all children progresses combined.
