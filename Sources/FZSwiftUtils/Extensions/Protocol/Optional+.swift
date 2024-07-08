2f3805309732429d15d04c6aa7dc710e343bf89f
Florian Zand <florianzand@me.com>
2024-07-08 04:13:06 +0200

    updated

--- Sources/FZSwiftUtils/Extensions/Protocol/Optional+.swift
+++ Sources/FZSwiftUtils/Extensions/Protocol/Optional+.swift
@@ -71,3 +71,19 @@ public extension Optional {
     }
 }
 
+/// A type represeting an optional value.
+public protocol OptionalProtocol: ExpressibleByNilLiteral {
+    associatedtype Wrapped
+    /// The optional value.
+    var optional: Wrapped? { get }
+}
+
+extension Optional: OptionalProtocol {
+    /// The optional value.
+    public var optional: Self { self }
+    
+    /// A Boolean value that indicates whether the optional value is `nil`.
+    var isNil: Bool {
+        self.optional == nil
+    }
+}
