a97d7912bbca4902ade35a9f8ddffde5d87d294e
Florian Zand <florianzand@me.com>
2024-07-19 17:20:38 +0200

    Create NSRectEdge+.swift

---
+++ Sources/FZSwiftUtils/Extensions/NSRectEdge+.swift
@@ -0,0 +1,33 @@
+//
+//  NSRectEdge+.swift
+//
+//
+//  Created by Florian Zand on 15.12.23.
+//
+
+#if os(macOS)
+    import Foundation
+
+    extension NSRectEdge {
+        /// The bottom edge of the rectangle.
+        public static var bottom: NSRectEdge {
+            .minY
+        }
+
+        /// The right edge of the rectangle.
+        public static var right: NSRectEdge {
+            .maxX
+        }
+
+        /// The top edge of the rectangle.
+        public static var top: NSRectEdge {
+            .maxY
+        }
+
+        /// The left edge of the rectangle.
+        public static var left: NSRectEdge {
+            .minX
+        }
+    }
+
+#endif
