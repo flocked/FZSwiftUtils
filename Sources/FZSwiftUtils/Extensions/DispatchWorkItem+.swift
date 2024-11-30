037b60f838776ef7f3ab71011c89a97eab12ca44
Florian Zand <florianzand@me.com>
2024-11-30 18:07:27 +0100

    Update DispatchWorkItem+.swift

--- Sources/FZSwiftUtils/Extensions/DispatchWorkItem+.swift
+++ Sources/FZSwiftUtils/Extensions/DispatchWorkItem+.swift
@@ -8,10 +8,17 @@
 import Foundation
 
 extension DispatchWorkItem {
-    /// Executes the work item's block asynchronously after the specified delay on the current thread.
+    /// Executes the work item's block asynchronously on the main thread after the specified delay.
     @discardableResult
     public func perform(after delay: TimeInterval) -> Self {
         DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: self)
+        return self
+    }
+    
+    /// Executes the work item's block asynchronously on the background thread after the specified delay.
+    @discardableResult
+    public func performBackground(after delay: TimeInterval) -> Self {
+        DispatchQueue.global().asyncAfter(deadline: .now() + delay, execute: self)
         return self
     }
 }
