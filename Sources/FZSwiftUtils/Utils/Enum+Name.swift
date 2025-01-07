813dfec760d156de37f4a43824ba73822ab2e516
Florian Zand <florianzand@me.com>
2025-01-07 17:48:21 +0100

    Update Enum+Name.swift

--- Sources/FZSwiftUtils/Utils/Enum+Name.swift
+++ Sources/FZSwiftUtils/Utils/Enum+Name.swift
@@ -10,6 +10,7 @@ import Foundation
 @_silgen_name("swift_EnumCaseName")
 func _getEnumCaseName<T>(_ value: T) -> UnsafePointer<CChar>?
 
+/// Returns the name of an enum case.
 public func getEnumCaseName<T>(for value: T) -> String? {
     if let stringPtr = _getEnumCaseName(value) {
         return String(validatingUTF8: stringPtr)
