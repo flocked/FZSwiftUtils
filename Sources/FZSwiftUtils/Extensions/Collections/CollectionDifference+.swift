3d3c17661294c6259f357fbb15a46e8ac994bb54
Florian Zand <florianzand@me.com>
2025-01-17 02:36:11 +0100

    Update CollectionDifference+.swift

--- Sources/FZSwiftUtils/Extensions/Collections/CollectionDifference+.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/CollectionDifference+.swift
@@ -8,7 +8,47 @@
 import Foundation
 
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-excerpt
+extension CollectionDifference where ChangeElement: Hashable {
+    
+    public enum ChangeStep {
+        case insert(_ element: ChangeElement, at: Int)
+        case remove(_ element: ChangeElement, at: Int)
+        case move(_ element: ChangeElement, from: Int, to: Int)
+    }
+    
+    public var steps: [ChangeStep] {
+        guard !isEmpty else { return [] }
+        
+        var steps: [ChangeStep] = []
+        var offsetTracker = RemainingRemovalTracker()
+
+        inferringMoves().forEach { change in
+            switch change {
+            case let .remove(offset, element, associatedWith):
+                if associatedWith != nil {
+                    offsetTracker.addSkippedRemoval(atOffset: offset)
+                } else {
+                    steps.append(.remove(element, at: offset))
+                    offsetTracker.removalMade(at: offset)
+                }
+
+            case let.insert(offset, element, associatedWith):
+                if let associatedWith = associatedWith {
+                    let from = offsetTracker.useSkippedRemoval(withOriginalOffset: associatedWith)
+                    let to = offsetTracker.adjustedInsertion(withOriginalOffset: offset)
+                    steps.append(.move(element, from: from, to: to))
+                    offsetTracker.insertionMade(at: to)
+                } else {
+                    let to = offsetTracker.adjustedInsertion(withOriginalOffset: offset)
+                    steps.append(.insert(element, at: to))
+                    offsetTracker.insertionMade(at: to)
+                }
+            }
+        }
+
+        return steps
+    }
+}
 
 private typealias RemainingRemovalTracker = [Int:Int]
 
