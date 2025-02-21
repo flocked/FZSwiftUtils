8b932b2a03038d311b2b9b8c7e9730547993ae0b
Florian Zand <florianzand@me.com>
2025-02-21 22:58:02 +0100

    Update CollectionDifference+.swift

--- Sources/FZSwiftUtils/Extensions/Collections/CollectionDifference+.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/CollectionDifference+.swift
@@ -7,21 +7,50 @@
 
 import Foundation
 
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-extension CollectionDifference where ChangeElement: Hashable {
+extension CollectionDifference.Change {
+    /// The offset at which the change occurs.
+    public var offset: Int {
+        switch self {
+        case let .insert(offset, _, _): return offset
+        case let .remove(offset, _, _): return offset
+        }
+    }
     
+    /// The element associated with the change.
+    public var element: ChangeElement {
+        switch self {
+        case let .insert(_, element, _): return element
+        case let .remove(_, element, _): return element
+        }
+    }
+    
+    /// The associated offset for matching insertions and removals, if available.
+    public var associatedOffset: Int? {
+        switch self {
+        case let .insert(_, _, associate): return associate
+        case let .remove(_, _, associate): return associate
+        }
+    }
+}
+
+extension CollectionDifference where ChangeElement: Hashable {
+    /// A value representing the different types of changes that can occur in a collection difference.
     public enum ChangeStep {
+        /// Inserts an element at a specific index.
         case insert(_ element: ChangeElement, at: Int)
+        /// Removes an element from a specific index.
         case remove(_ element: ChangeElement, at: Int)
+        /// Moves an element from one index to another.
         case move(_ element: ChangeElement, from: Int, to: Int)
     }
     
+    /// Returns the ordered steps required to transform one collection into another.
     public var steps: [ChangeStep] {
         guard !isEmpty else { return [] }
         
         var steps: [ChangeStep] = []
         var offsetTracker = RemainingRemovalTracker()
-
+        
         inferringMoves().forEach { change in
             switch change {
             case let .remove(offset, element, associatedWith):
@@ -31,7 +60,6 @@ extension CollectionDifference where ChangeElement: Hashable {
                     steps.append(.remove(element, at: offset))
                     offsetTracker.removalMade(at: offset)
                 }
-
             case let.insert(offset, element, associatedWith):
                 if let associatedWith = associatedWith {
                     let from = offsetTracker.useSkippedRemoval(withOriginalOffset: associatedWith)
@@ -45,7 +73,6 @@ extension CollectionDifference where ChangeElement: Hashable {
                 }
             }
         }
-
         return steps
     }
 }
@@ -53,7 +80,6 @@ extension CollectionDifference where ChangeElement: Hashable {
 private typealias RemainingRemovalTracker = [Int:Int]
 
 private extension RemainingRemovalTracker {
-    
     mutating func addSkippedRemoval(atOffset offset: Int) {
         self[offset] = offset }
     
@@ -73,14 +99,12 @@ private extension RemainingRemovalTracker {
             if value >= offset {
                 self[key] = value + 1 } }
     }
-
+    
     func adjustedInsertion(withOriginalOffset originalOffset: Int) -> Int {
         var adjustedOffset = originalOffset
-
         values.sorted().forEach { offset in
             if offset <= adjustedOffset {
                 adjustedOffset += 1 } }
-        
         return adjustedOffset
     }
 }
