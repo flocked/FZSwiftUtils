f304f2fb0a6df34b923ca07a5a27752a32fbbc1b
Florian Zand <florianzand@me.com>
2025-03-04 01:30:04 +0100

    Update NSKeyedUnarchiver+.swift

--- Sources/FZSwiftUtils/Extensions/NSKeyedUnarchiver+.swift
+++ Sources/FZSwiftUtils/Extensions/NSKeyedUnarchiver+.swift
@@ -16,13 +16,25 @@ extension NSKeyedUnarchiver {
      - Throws: If the data isn't an archive, doesn't contain the object or the decoding failed.
      */
     public static func unarchive<Object: NSObject>(_ data: Data, requiresSecureCoding: Bool = false) throws -> Object where Object: NSCoding {
+        let value = try unarchive(data)
+        guard let value = value as? Object else {
+            throw UnarchiverErrors.wrongMapping(original: type(of: value), new: Object.self)
+        }
+        return value
+    }
+    
+    /**
+     Decodes a previously-archived object.
+     
+     - Parameter data: Data of an object previously encoded by `NSKeyedArchiver`.
+     - Returns: The decoded object.
+     - Throws: If the data isn't an archive, doesn't contain the object or the decoding failed.
+     */
+    public static func unarchive(_ data: Data, requiresSecureCoding: Bool = false) throws -> AnyObject {
         let unarchiver = try NSKeyedUnarchiver(forReadingFrom: data)
         unarchiver.requiresSecureCoding = requiresSecureCoding
         guard let value = unarchiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as? AnyObject else {
             throw UnarchiverErrors.failedDecoding
-        }
-        guard let value = value as? Object else {
-            throw UnarchiverErrors.wrongMapping(original: type(of: value), new: Object.self)
         }
         return value
     }
