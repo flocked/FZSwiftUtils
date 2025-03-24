1cd9cc44969b7885753ee928b66d9c3c9bd7213d
Florian Zand <florianzand@me.com>
2025-03-24 20:34:11 +0100

    Create OptionSetDescribable.swift

---
+++ Sources/FZSwiftUtils/Extensions/Protocol/OptionSetDescribable.swift
@@ -0,0 +1,22 @@
+//
+//  OptionSetDescribable.swift
+//  
+//
+//  Created by Florian Zand on 24.03.25.
+//
+
+import Foundation
+
+/// An `OptionSet` that provides description listing all elements.
+public protocol OptionSetDescribable: OptionSet, CustomStringConvertible where RawValue: BinaryInteger {
+    static var allCases: [(Element, String)] { get }
+}
+
+extension OptionSetDescribable {
+    public var description: String {
+        let names = Self.allCases
+            .compactMap { contains($0.0) ? $0.1 : nil }
+            .joined(separator: ", ")
+        return "[\(names)]"
+    }
+}
