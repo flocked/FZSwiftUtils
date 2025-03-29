1b11814dc210aa75f2e775338aff51d97e24b756
Florian Zand <florianzand@me.com>
2025-03-29 22:41:44 +0100

    updated

---
+++ Sources/FZSwiftUtils/Extensions/File & URL/URL+DuplicateFiles.swift
@@ -0,0 +1,75 @@
+//
+//  URL+DuplicateFiles.swift
+//
+//
+//  Created by Florian Zand on 29.03.25.
+//
+
+import Foundation
+
+extension URL {
+    /**
+     Returns the duplicate files for the specified file URLs.
+     
+     - Parameter urls: The file URLs to search duplicates.
+     - Returns: The duplicate files sorted by file size and the non-duplicate files.
+     */
+    public static func findDuplicateFiles(for urls: [URL]) -> (duplicates: [[HashedFile]], nonDuplicates: [HashedFile]) {
+        let found = findDuplicateFiles(for: urls.map({ HashedFile($0) }))
+        let duplicates = found.duplicates
+        let nonDuplicates = found.nonDuplicates
+        return (duplicates, nonDuplicates)
+    }
+    
+    private static func findDuplicateFiles(for urls: [HashedFile]) -> (duplicates: [[HashedFile]], nonDuplicates: [HashedFile]) {
+        let filesBySize = Dictionary(grouping: urls, by: \.url.resources.fileSize)
+        var nonDuplicates = filesBySize[nil] ?? []
+        var duplicates: [[HashedFile]] = []
+        for files in filesBySize.filter({$0.key != nil}).values {
+            if files.count > 1 {
+                let filesByHash = Dictionary(grouping: files, by: \.hash )
+                nonDuplicates += filesByHash[nil] ?? []
+                for filesHashes in filesByHash {
+                    if filesHashes.key != nil {
+                        if filesHashes.value.count > 1 {
+                            duplicates += filesHashes.value
+                        } else {
+                            nonDuplicates += filesHashes.value
+                        }
+                    }
+                }
+            } else {
+                nonDuplicates += files
+            }
+        }
+        duplicates = duplicates.sorted(by: \.first?.url.resources.fileSize, .smallestFirst)
+        return (duplicates, nonDuplicates)
+    }
+}
+
+/// A hashed file.
+public class HashedFile: Equatable {
+    private var _hash: OSHash?
+    private var didCalculateHash = false
+    
+    /// The URL of the file.
+    public let url: URL
+
+    /// The hash of the file.
+    public var hash: OSHash? {
+        if !didCalculateHash {
+            _hash = try? OSHash(url: url)
+            didCalculateHash = true
+        }
+        return _hash
+    }
+    
+    /// Creates a hashed file for the specified file URL.
+    public init(_ url: URL) {
+        self.url = url
+    }
+    
+    public static func == (lhs: HashedFile, rhs: HashedFile) -> Bool {
+        lhs.hash != nil && lhs.hash == rhs.hash
+    }
+}
--- Sources/FZSwiftUtils/Utils/OSHash.swift
+++ Sources/FZSwiftUtils/Utils/OSHash.swift
@@ -18,7 +18,7 @@ public struct OSHash: Hashable {
     
     /// The number of bytes that represents the hash function’s internal state.
     static let blockByteCount: Int = 65536
-  
+    
     /// The number of bytes in a OSHash digest.
     static let byteCount = 8
     
@@ -29,196 +29,49 @@ public struct OSHash: Hashable {
     }
     
     /**
-     Computes the OSHash digest for the specified file.
+     Computes the `OSHash` digest for the specified file.
      
      - Throws: Throws if the file isn't available, can't be accessed or is to small.
      */
     public init(url: URL) throws {
-        let fileHandler = try FileHandle(forReadingFrom: url)
-        let startData: Data = fileHandler.readData(ofLength: Self.blockByteCount) as Data
-
-        fileHandler.seekToEndOfFile()
-        let fileSize: UInt64 = fileHandler.offsetInFile
-        
-        guard UInt64(Self.blockByteCount) <= fileSize else {
-            fileHandler.closeFile()
+        let fileSize = try FileManager.default.attributesOfItem(atPath: url.path)[.size] as! UInt64
+        guard fileSize >= UInt64(Self.blockByteCount) else {
             self = Self(digest: fileSize)
             return
         }
         
-        fileHandler.seek(toFileOffset: max(0, fileSize - UInt64(Self.blockByteCount)))
-        let endData: Data = fileHandler.readData(ofLength: Self.blockByteCount)
+        let fileHandler = try FileHandle(forReadingFrom: url)
+        defer { fileHandler.closeFile() }
+        
+        let startData = fileHandler.readData(ofLength: Self.blockByteCount)
+        fileHandler.seek(toFileOffset: fileSize - UInt64(Self.blockByteCount))
+        let endData = fileHandler.readData(ofLength: Self.blockByteCount)
+        
         self = Self.hash(size: fileSize, startData: startData, endData: endData)
     }
     
-    /// Computes the OSHash digest for the specified data.
+    /// Computes the `OSHash` digest for the specified data.
     public init<D: DataProtocol>(data: D) {
-        let data = Data(data)
         let size = UInt64(data.count)
-        let startData = data[0 ..< Self.blockByteCount]
-        let endData = data[(data.count - 1 - Self.blockByteCount) ... data.count - 1]
-        self = Self.hash(size: size, startData: startData, endData: endData)
-    }
-    
-    static func hash(size: UInt64, startData: Data, endData: Data) -> Self {
-        var hash = size
-        guard UInt64(Self.blockByteCount) <= size else {
-            return Self(digest: hash)
+        guard size >= UInt64(Self.blockByteCount) else {
+            self = Self(digest: size)
+            return
         }
         
-        startData.withUnsafeBytes { buffer in
-            let binded = buffer.bindMemory(to: UInt64.self)
-            let data_bytes = UnsafeBufferPointer<UInt64>(
-                start: binded.baseAddress,
-                count: startData.count / MemoryLayout<UInt64>.size
-            )
-            hash = data_bytes.reduce(hash,&+)
+        let startData = data.prefix(Self.blockByteCount)
+        let endData = data.suffix(Self.blockByteCount)
+        
+        self = Self.hash(size: size, startData: Data(startData), endData: Data(endData))
+    }
+    
+    private static func hash(size: UInt64, startData: Data, endData: Data) -> Self {
+        var hash = size
+        hash = startData.withUnsafeBytes { buffer in
+            buffer.bindMemory(to: UInt64.self).reduce(hash, &+)
         }
-        endData.withUnsafeBytes { buffer in
-            let binded = buffer.bindMemory(to: UInt64.self)
-            let data_bytes = UnsafeBufferPointer<UInt64>(
-                start: binded.baseAddress,
-                count: endData.count / MemoryLayout<UInt64>.size
-            )
-            hash = data_bytes.reduce(hash,&+)
+        hash = endData.withUnsafeBytes { buffer in
+            buffer.bindMemory(to: UInt64.self).reduce(hash, &+)
         }
         return Self(digest: hash)
     }
 }
-
-
-/*
-import CryptoKit
-
-/**
- An implementation of the OpenSubtitle hash.
- 
- The OSHash is calculated via the provided file size + the checksum of the first and last 64k bytes (even if they overlap because the file/data is smaller than 128k bytes).
- 
- To calculate the hash for a file, use ``hash(url:)`` or data ``hash(data:)-73qi1``.
- */
-public struct OSHash: HashFunction {
-    
-    /// The number of bytes that represents the hash function’s internal state.
-    public static let blockByteCount: Int = 65536
-  
-    /// The number of bytes in a OSHash digest.
-    public static let byteCount = 8
-    
-    /// The digest type for a OSHash hash function.
-    public typealias Digest = OSHashDigest
-    
-    let digest: Data = Data()
-    
-    /// Creates a OSHash hash function.
-    public init() {
-    }
-    
-    /**
-     Computes the OSHash digest for the specified file.
-     
-     - Throws: Throws if the file isn't available, can't be accessed or is to small.
-     */
-    public static func hash(url: URL) throws -> OSHashDigest {
-        let fileHandler = try FileHandle(forReadingFrom: url)
-        let startData: Data = fileHandler.readData(ofLength: blockByteCount) as Data
-
-        fileHandler.seekToEndOfFile()
-        let fileSize: UInt64 = fileHandler.offsetInFile
-        
-        guard UInt64(blockByteCount) <= fileSize else {
-            fileHandler.closeFile()
-            return OSHashDigest(digest: fileSize)
-        }
-        
-        fileHandler.seek(toFileOffset: max(0, fileSize - UInt64(blockByteCount)))
-        let endData: Data = fileHandler.readData(ofLength: blockByteCount)
-        return hash(size: fileSize, startData: startData, endData: endData)
-    }
-    
-    /// Computes the OSHash digest for the specified data.
-    public static func hash<D: DataProtocol>(data: D) -> OSHashDigest {
-        let data = Data(data)
-        let size = UInt64(data.count)
-        let startData = data[0 ..< blockByteCount]
-        let endData = data[(data.count - 1 - blockByteCount) ... data.count - 1]
-        return hash(size: size, startData: startData, endData: endData)
-    }
-    
-    static func hash(size: UInt64, startData: Data, endData: Data) -> OSHashDigest {
-        var hash = size
-        guard UInt64(blockByteCount) <= size else {
-            return OSHashDigest(digest: hash)
-        }
-        
-        startData.withUnsafeBytes { buffer in
-            let binded = buffer.bindMemory(to: UInt64.self)
-            let data_bytes = UnsafeBufferPointer<UInt64>(
-                start: binded.baseAddress,
-                count: startData.count / MemoryLayout<UInt64>.size
-            )
-            hash = data_bytes.reduce(hash,&+)
-        }
-        endData.withUnsafeBytes { buffer in
-            let binded = buffer.bindMemory(to: UInt64.self)
-            let data_bytes = UnsafeBufferPointer<UInt64>(
-                start: binded.baseAddress,
-                count: endData.count / MemoryLayout<UInt64>.size
-            )
-            hash = data_bytes.reduce(hash,&+)
-        }
-        return OSHashDigest(digest: hash)
-    }
-    
-    /// Not implemented. Use either ``hash(data:)`` or ``hash(url:)``.
-    public mutating func update(bufferPointer: UnsafeRawBufferPointer) {
-        
-    }
-    
-    /// Not implemented. Use either ``hash(data:)`` or ``hash(url:)``.
-    public func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
-        try digest.withUnsafeBytes(body)
-    }
-    
-    /// Not implemented. Use either ``hash(data:)`` or ``hash(url:)``.
-    public func finalize() -> OSHashDigest {
-        OSHashDigest(digest: digest)
-    }
-}
-
-/// The output of a OpenSubtitle hash.
-public struct OSHashDigest: Digest {
-    
-    private let digest: Data
-
-    public static var byteCount: Int = 8
-
-    init(digest: Data) {
-        self.digest = digest
-    }
-    
-    init(digest: UInt64) {
-        let byteArray = Swift.withUnsafeBytes(of: digest.bigEndian) {
-            Array($0)
-        }
-        self.digest = Data(byteArray)
-    }
-    
-    
-    public func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
-        try digest.withUnsafeBytes(body)
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(digest)
-    }
-    
-    public var description: String {
-        hexString
-    }
-    
-    public func makeIterator() -> Data.Iterator {
-        digest.makeIterator()
-    }
-}
- */
