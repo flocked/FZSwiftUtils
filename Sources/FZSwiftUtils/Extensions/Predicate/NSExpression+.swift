6a3ec195c525ce7b09a850667f7f9aed5f805716
Florian Zand <florianzand@me.com>
2025-04-17 16:07:58 +0200

    Update NSExpression+.swift

--- Sources/FZSwiftUtils/Extensions/Predicate/NSExpression+.swift
+++ Sources/FZSwiftUtils/Extensions/Predicate/NSExpression+.swift
@@ -8,6 +8,31 @@
 import Foundation
 
 public extension NSExpression {
+    /// Creates an expression using the specificed key path.
+    static func keyPath(_ keyPath: String) -> NSExpression {
+        NSExpression(forKeyPath: keyPath)
+    }
+    
+    /// Creates an expression using the specificed key path.
+    static func keyPath<Root, Value>(_ keyPath: KeyPath<Root, Value>) -> NSExpression {
+        NSExpression(forKeyPath: keyPath)
+    }
+        
+    static func variable(_ keyPath: String) -> NSExpression {
+        NSExpression(forVariable: keyPath)
+    }
+    
+    /// Creates an expression that represents the specified constant value.
+    static func constant(_ value: Any?) -> NSExpression {
+        NSExpression(forConstantValue: value)
+    }
+    
+    /// Creates an expression that represents any key for a Spotlight query.
+    static var any: NSExpression {
+        NSExpression.expressionForAnyKey()
+    }
+    
+    /// Creates the expression with the expression format and arguments list you specify.
     convenience init(_ value: String, _ args: Any...) {
         self.init(format: value, args)
     }
