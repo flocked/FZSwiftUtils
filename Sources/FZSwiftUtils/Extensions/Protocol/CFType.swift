dbba4e16933337b79c836fd2fb0c9066c7cd7824
Florian Zand <florianzand@me.com>
2025-07-05 00:33:27 +0200

    Update CFType.swift

--- Sources/FZSwiftUtils/Extensions/Protocol/CFType.swift
+++ Sources/FZSwiftUtils/Extensions/Protocol/CFType.swift
@@ -13,8 +13,23 @@ import CoreGraphics
  
  This protocol allows extending Core Foundation / Core Graphics with custom initalizers which is normally not allowed.
  */
-public protocol CFType { }
+public protocol CFType {
+    static var typeID: CFTypeID { get }
+}
 
+// Core Graphics types
+extension CGColor: CFType { }
+extension CGColorSpace: CFType { }
+extension CGGradient: CFType { }
+extension CGPath: CFType { }
+extension CGImage: CFType { }
+
+#if os(macOS)
+import Carbon
+extension CGEvent: CFType { }
+#endif
+
+/*
 // Core Foundation types
 extension CFString: CFType { }
 extension CFNumber: CFType { }
@@ -25,20 +40,24 @@ extension CFDate: CFType { }
 extension CFURL: CFType { }
 extension CFBag: CFType { }
 extension CFData: CFType { }
+ */
 
-// Core Graphics types
-extension CGColor: CFType { }
-extension CGColorSpace: CFType { }
-extension CGGradient: CFType { }
-extension CGPath: CFType { }
+/*
 extension CGAffineTransform: CFType { }
 extension CGPoint: CFType { }
 extension CGSize: CFType { }
 extension CGRect: CFType { }
 extension CGVector: CFType { }
+ */
 
-#if os(macOS)
-import Carbon
-
-extension CGEvent: CFType { }
-#endif
+extension CFType {
+    public init?(_ value: Any) {
+        guard let value = value as? AnyObject, CFGetTypeID(value) == Self.typeID else { return nil }
+        self = value as! Self
+    }
+    
+    public init?(_ value: Any?) {
+        guard let value = value as? AnyObject, CFGetTypeID(value) == Self.typeID else { return nil }
+        self = value as! Self
+    }
+}
