ebf32209fe8558d2e6152015a109e70e8b649a6f
Florian Zand <florianzand@me.com>
2025-07-04 14:22:30 +0200

    Update Collection+Random.swift

--- Sources/FZSwiftUtils/Extensions/Collections/Collection+Random.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/Collection+Random.swift
@@ -9,51 +9,29 @@ import Foundation
 
 public extension Collection {
     /**
-     Returns an array of random elements of the collection.
+     Returns an array of random elements excluding the specified elements.
+
      - Parameters:
-        - amount: The amount of random elements.
-        - unique: A Boolean value indicating whether the returned elements should be unique (non repeating).
+        - amount: The number of elements to return.
+        - unique: A Boolean value indicating whether returned elements should be unique (non repeating).
+        - strategy: A sampling strategy to use.
      */
-    func randomElements(amount: Int, unique: Bool = true) -> [Element] {
-        if unique == false {
-            guard !isEmpty else { return [] }
-            return (0..<amount).reduce(into: [Element]()) { partialResult, _ in
-                partialResult.append(randomElement()!)
-            }
-        }
+    func randomElements(amount: Int, unique: Bool = true, strategy: SequenceSamplingStrategy = .shuffle) -> [Element] {
         guard amount > 0 else { return [] }
-        
         var rng = SystemRandomNumberGenerator()
         
-        var w = 1.0
-        var result: [Element] = []
-        result.reserveCapacity(amount)
-        
-        // Fill the reservoir with the first `count` elements.
-        var i = startIndex
-        while i != endIndex, result.count < amount {
-            result.append(self[i])
-            formIndex(after: &i)
+        if !unique {
+            return (0..<amount).compactMap { _ in randomElement(using: &rng) }
         }
-        
-        while i != endIndex {
-            // Calculate the next value of w.
-            w *= nextW(k: amount, using: &rng)
-            
-            // Find index of the next element to swap into the reservoir.
-            let offset = nextOffset(w: w, using: &rng)
-            i = index(i, offsetBy: offset, limitedBy: endIndex) ?? endIndex
-            
-            if i != endIndex {
-                // Swap selected element with a randomly chosen one in the reservoir.
-                let j = Int.random(in: 0..<result.count, using: &rng)
-                result[j] = self[i]
-                formIndex(after: &i)
-            }
+
+        switch strategy {
+        case .shuffle:
+            return shuffled().prefix(amount).map { $0 }
+        case .reservoir:
+            return reservoirSample(amount: amount, using: &rng)
+        case .vitter:
+            return vitterSample(amount: amount, using: &rng)
         }
-        
-        result.shuffle(using: &rng)
-        return result
     }
 }
 
@@ -61,7 +39,7 @@ public extension Collection where Element: Equatable {
     /**
      Returns a random element of the collection excluding the specified elements.
 
-     - Parameter excluding: The elements of the collection to be excluded from returning.
+     - Parameter excluding: The elements to be excluded from returning.
      */
     func randomElement(excluding: [Element]) -> Element? {
         shuffled().first(where: { !excluding.contains($0) })
@@ -71,78 +49,146 @@ public extension Collection where Element: Equatable {
      Returns an array of random elements excluding the specified elements.
 
      - Parameters:
-        - amount: The amount of random elements.
-        - excluding: The elements of the collection to be excluded from random.
-        - unique: A Boolean value indicating whether the returned elements should be unique (non repeating).
+        - amount: The number of elements to return.
+        - excluding: The elements to be excluded from returning.
+        - unique: A Boolean value indicating whether returned elements should be unique (non repeating).
+        - strategy: A sampling strategy to use.
      */
-    func randomElements(amount: Int, excluding: [Element], unique: Bool = true) -> [Element] {
+    func randomElements(amount: Int, excluding: [Element], unique: Bool = true, strategy: SequenceSamplingStrategy = .reservoir) -> [Element] {
         let elements: [Element] = filter { excluding.contains($0) == false }
-        return elements.randomElements(amount: amount, unique: unique)
+        return elements.randomElements(amount: amount, unique: unique, strategy: strategy)
     }
 }
 
 public extension Sequence {
     /**
-     Returns an array of random elements of the collection.
+     Returns an array of random elements from the sequence.
+
      - Parameters:
-        - amount: The amount of random elements.
-        - unique: A Boolean value indicating whether the returned elements should be unique (non repeating).
+        - amount: The number of elements to return.
+        - unique: A Boolean value indicating whether returned elements should be unique (non repeating).
+        - strategy: A sampling strategy to use.
+
+     - Returns: An array of randomly selected elements.
      */
-    func randomElements(amount: Int, unique: Bool = true) -> [Element] {
-        if unique == false {
-            return Array(self).randomElements(amount: amount)
-        }
+    func randomElements(amount: Int, unique: Bool = true, strategy: SequenceSamplingStrategy = .reservoir) -> [Element] {
         guard amount > 0 else { return [] }
-        
         var rng = SystemRandomNumberGenerator()
         
-        var w = 1.0
-        var result: [Element] = []
-        result.reserveCapacity(amount)
-        
-        // Fill the reservoir with the first `k` elements.
-        var iterator = makeIterator()
-        while result.count < amount, let el = iterator.next() {
-            result.append(el)
+        if !unique {
+            let elements = Array(self)
+            guard !elements.isEmpty else { return [] }
+            return (0..<amount).compactMap { _ in elements.randomElement(using: &rng) }
         }
         
+        switch strategy {
+        case .shuffle:
+            return shuffled().prefix(amount).map { $0 }
+        case .reservoir:
+            return reservoirSample(amount: amount, using: &rng)
+        case .vitter:
+            return vitterSample(amount: amount, using: &rng)
+        }
+    }
+}
+
+/// Strategy for selecting a unique random sample from a sequence.
+public enum SequenceSamplingStrategy {
+    /**
+     Reservoir sampling algorithm (`R`).
+     
+     A simple and efficient algorithm for streaming data or when the sequence size is unknown.
+     
+     Every item has an equal probability of being included in the result.
+     */
+    case reservoir
+    /**
+     Vitter sampling algorithm (`X`).
+     
+     It is more efficient compared to ``reservoir`` when the desired sample size is much smaller than the total sequence size.
+     
+     It skips over elements probabilistically to reduce iteration steps.
+     */
+    case vitter
+    /**
+     Full shuffle.
+     
+     Fastest algorithm if the collection is already in memory and has known count.
+     */
+    case shuffle
+}
+
+fileprivate extension Sequence {
+    func reservoirSample<G: RandomNumberGenerator>(amount: Int, using rng: inout G) -> [Element] {
+        var reservoir: [Element] = []
+        reservoir.reserveCapacity(amount)
+
+        var iterator = makeIterator()
+        var i = 0
+
+        while i < amount, let next = iterator.next() {
+            reservoir.append(next)
+            i += 1
+        }
+
+        guard i == amount else {
+            // Fewer elements than amount
+            return reservoir
+        }
+
+        while let next = iterator.next() {
+            i += 1
+            let j = Int.random(in: 0..<i, using: &rng)
+            if j < amount {
+                reservoir[j] = next
+            }
+        }
+
+        return reservoir
+    }
+
+    func vitterSample<G: RandomNumberGenerator>(amount: Int, using rng: inout G) -> [Element] {
+        var result: [Element] = []
+        result.reserveCapacity(amount)
+
+        var iterator = makeIterator()
+        var i = 0
+
+        // Fill the reservoir
+        while i < amount, let el = iterator.next() {
+            result.append(el)
+            i += 1
+        }
+
+        guard i == amount else {
+            return result // not enough elements
+        }
+
+        var w = 1.0
+
         while true {
-            // Calculate the next value of w.
-            w *= nextW(k: amount, using: &rng)
-            
-            // Find the offset of the next element to swap into the reservoir.
-            var offset = nextOffset(w: w, using: &rng)
-            
-            // Skip over `offset` elements to find the selected element.
+            w *= Self.nextW(k: amount, using: &rng)
+            var offset = Self.nextOffset(w: w, using: &rng)
+
             while offset > 0, let _ = iterator.next() {
                 offset -= 1
             }
-            guard let nextElement = iterator.next() else { break }
-            
-            // Swap selected element with a randomly chosen one in the reservoir.
-            let j = Int.random(in: 0..<result.count, using: &rng)
-            result[j] = nextElement
+
+            guard let next = iterator.next() else { break }
+
+            let j = Int.random(in: 0..<amount, using: &rng)
+            result[j] = next
         }
-        result.shuffle(using: &rng)
+
         return result
     }
-}
 
-extension Sequence {
-    func nextW<G: RandomNumberGenerator>(k: Int, using rng: inout G) -> Double {
-        Double.root(.random(in: 0..<1, using: &rng), k)
+    static func nextW<G: RandomNumberGenerator>(k: Int, using rng: inout G) -> Double {
+        pow(Double.random(in: 0..<1, using: &rng), 1.0 / Double(k))
     }
-    
-    func nextOffset<G: RandomNumberGenerator>(w: Double, using rng: inout G) -> Int {
+
+    static func nextOffset<G: RandomNumberGenerator>(w: Double, using rng: inout G) -> Int {
         let offset = log(Double.random(in: 0..<1, using: &rng)) / log1p(-w)
         return offset < Double(Int.max) ? Int(offset) : Int.max
-    }
-}
-
-extension Double {
-    static func root(_ x: Double, _ n: Int) -> Double {
-        guard x >= 0 || n % 2 != 0 else { return .nan }
-        if n == 3 { return cbrt(x) }
-        return Double(signOf: x, magnitudeOf: pow(x.magnitude, 1/Double(n)))
     }
 }
