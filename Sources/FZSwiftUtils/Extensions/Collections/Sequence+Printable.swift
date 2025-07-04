4674a8f2d5995f246a5fa50481f70dc01f7d1864
Florian Zand <florianzand@me.com>
2025-07-04 13:44:22 +0200

    Update Sequence+Printable.swift

--- Sources/FZSwiftUtils/Extensions/Collections/Sequence+Printable.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/Sequence+Printable.swift
@@ -10,8 +10,60 @@ import Foundation
 
 extension Sequence {
     /**
-     Returns a formatted string representation of the sequence.
+     Writes the textual representations of each element of the sequence into the standard output.
+     
+     - Parameters:
+        - separator: A string to print between each item. The default is a single space (`" "`).
+        - terminator: The string to print after all items have been printed. The default is a newline (`"\n"`).
+        - includeIndex: A Boolean value indicating whether to include the index of each element.
+     */
+    public func printEach(separator: String = " ", terminator: String = "\n") {
+        forEach({ print($0, separator: separator, terminator: terminator) })
+    }
+    
+    /**
+     Writes the textual representations of each element of the sequence for debugging into the standard output.
+     
+     - Parameters:
+        - separator: A string to print between each item. The default is a single space (`" "`).
+        - terminator: The string to print after all items have been printed. The default is a newline (`"\n"`).
+        - includeIndex: A Boolean value indicating whether to include the index of each element.
+     */
+    public func debugPrintEach(separator: String = " ", terminator: String = "\n") {
+        forEach({ debugPrint($0, separator: separator, terminator: terminator) })
+    }
+}
 
+extension Collection {
+    /**
+     Writes the textual representations of each element of the collection into the standard output.
+     
+     - Parameters:
+        - includeIndex: A Boolean value indicating whether to include the index of each element.
+        - separator: A string to print between each element. The default is a single space (`" "`).
+        - terminator: The string to print after all element have been printed. The default is a newline (`"\n"`).
+     */
+    public func printEach(includeIndex: Bool, separator: String = " ", terminator: String = "\n") {
+        includeIndex ? indexed().forEach({ print("\($0.index): \($0.element)", separator: separator, terminator: terminator) }) : forEach({ print($0, separator: separator, terminator: terminator) })
+    }
+    
+    /**
+     Writes the textual representations of each element of the collection for debugging into the standard output.
+     
+     - Parameters:
+        - includeIndex: A Boolean value indicating whether to include the index of each element.
+        - separator: A string to print between each element. The default is a single space (`" "`).
+        - terminator: The string to print after all element have been printed. The default is a newline (`"\n"`).
+     */
+    public func debugPrintEach(includeIndex: Bool, separator: String = " ", terminator: String = "\n") {
+        includeIndex ? indexed().forEach({ debugPrint("\($0.index): \($0.element)", separator: separator, terminator: terminator) }) : forEach({ debugPrint($0, separator: separator, terminator: terminator) })
+    }
+}
+
+extension Sequence {
+    /**
+     Returns a formatted string representation of the sequence.
+     
      - Parameter newLines: A `Boolean` value indicating whether to format each element on a new line.
      */
     public func printableDescription(newLines: Bool = false) -> String {
