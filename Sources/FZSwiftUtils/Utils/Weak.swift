2a2574d538f460233bd69840d40e6a8a47faee4c
Florian Zand <florianzand@me.com>
2025-07-30 07:58:30 +0200

    updated

--- Sources/FZSwiftUtils/Utils/Weak.swift
+++ Sources/FZSwiftUtils/Utils/Weak.swift
@@ -51,12 +51,30 @@ public extension Array where Element: WeakReference {
     mutating func reap() {
         self = filter { $0.object != nil }
     }
+    
+    /**
+     Creates an array containing the elements of a sequence.
+     
+     - Parameter elements: The sequence of elements to turn into an array.
+     */
+    init<S>(_ elements: S) where S : Sequence, Self.Element.Object == S.Element {
+        self = Self(elements.map({Element($0)}))
+    }
 }
 
 public extension Set where Element: WeakReference {
     /// Removes all weak objects that are `nil`.
     mutating func reap() {
         self = filter { $0.object != nil }
+    }
+    
+    /**
+     Creates a set containing the elements of a sequence.
+     
+     - Parameter elements: The sequence of elements to turn into a set.
+     */
+    init<S>(_ elements: S) where S : Sequence, Self.Element.Object == S.Element {
+        self = Self(elements.map({Element($0)}))
     }
 }
 
@@ -69,6 +87,71 @@ public extension Dictionary where Value: WeakReference {
     /// The dictionary with values whose weak object isn't `nil`.
     var nonNil: [Key: Value.Object] {
         compactMapValues({ $0.object })
+    }
+    
+    /**
+     Creates a new dictionary from the key-value pairs in the given sequence.
+     
+     - Parameter keysAndValues: A sequence of key-value pairs to use for the new dictionary. Every key in keysAndValues must be unique.
+     */
+    init<S>(uniqueKeysWithValues keysAndValues: S) where S : Sequence, S.Element == (Key, Value.Object) {
+        self = Self(uniqueKeysWithValues: keysAndValues.map({ ($0.0, Value($0.1)) }))
+    }
+    
+    /**
+     Creates a new dictionary from the key-value pairs in the given sequence, using a combining closure to determine the value for any duplicate keys.
+     
+     - Parameters:
+        - keysAndValues: A sequence of key-value pairs to use for the new dictionary.
+        - combine: A closure that is called with the values for any duplicate keys that are encountered. The closure returns the desired value for the final dictionary.
+     */
+    init<S>(_ keysAndValues: S, uniquingKeysWith combine: (Value.Object, Value.Object) throws -> Value) rethrows where S : Sequence, S.Element == (Key, Value.Object) {
+        var result: [Key: Value] = [:]
+        for (key, object) in keysAndValues {
+            if let existing = result[key]?.object {
+                result[key] = try combine(existing, object)
+            } else {
+                result[key] = Value(object)
+            }
+        }
+        self = result
+    }
+}
+
+public extension Dictionary where Key: WeakReference {
+    /**
+     Creates a new dictionary from the key-value pairs in the given sequence.
+     
+     - Parameter keysAndValues: A sequence of key-value pairs to use for the new dictionary. Every key in keysAndValues must be unique.
+     */
+    init<S>(uniqueKeysWithValues keysAndValues: S) where S : Sequence, S.Element == (Key.Object, Value) {
+        self = Self(uniqueKeysWithValues: keysAndValues.map({ (Key($0.0), $0.1) }))
+    }
+    
+    /**
+     Creates a new dictionary from the key-value pairs in the given sequence, using a combining closure to determine the value for any duplicate keys.
+     
+     - Parameters:
+        - keysAndValues: A sequence of key-value pairs to use for the new dictionary.
+        - combine: A closure that is called with the values for any duplicate keys that are encountered. The closure returns the desired value for the final dictionary.
+     */
+    init<S>(_ keysAndValues: S, uniquingKeysWith combine: (Value, Value) throws -> Value) rethrows where S : Sequence, S.Element == (Key.Object, Value) {
+        self = try Self(keysAndValues.map({ (Key($0.0), $0.1) }), uniquingKeysWith: combine)
+    }
+    
+    /**
+     Creates a new dictionary whose keys are the groupings returned by the given closure and whose values are arrays of the elements that returned each key.
+     
+     - Parameters:
+        - values: A sequence of values to group into a dictionary.
+        - keyForValue: A closure that returns a key for each element in values.
+     */
+    init<S>(grouping values: S, by keyForValue: (S.Element) throws -> Key.Object
+    ) rethrows where Value == [S.Element], S : Sequence {
+        self.init()
+        for value in values {
+            self[Key(try keyForValue(value)), default: []].append(value)
+        }
     }
 }
 
