057b78904456406c964505c1a3943c47fcb17a47
Florian Zand <florianzand@me.com>
2025-08-17 05:41:36 +0200

    Update CFType.swift

--- Sources/FZSwiftUtils/Extensions/Protocol/CFType.swift
+++ Sources/FZSwiftUtils/Extensions/Protocol/CFType.swift
@@ -9,11 +9,12 @@ import Foundation
 import CoreGraphics
 
 /**
- A Core Foundation / Core Graphics type.
+ A Core Foundation type.
  
- This protocol allows extending Core Foundation / Core Graphics with custom initalizers which is normally not allowed.
+ This protocol allows extending Core Foundation types with custom initalizers which is normally not allowed.
  */
 public protocol CFType {
+    /// The Core Foundation type identifier.
     static var typeID: CFTypeID { get }
 }
 
@@ -29,30 +30,39 @@ extension CGEvent: CFType { }
 #endif
 
 extension CFString: CFType {
+    /// Returns the Core Foundation type identifier for a `CFString` type.
     public static var typeID: CFTypeID { CFStringGetTypeID() }
 }
 extension CFNumber: CFType {
+    /// Returns the Core Foundation type identifier for a `CFNumber` type.
     public static var typeID: CFTypeID { CFNumberGetTypeID() }
 }
 extension CFArray: CFType {
+    /// Returns the Core Foundation type identifier for a `CFArray` type.
     public static var typeID: CFTypeID { CFArrayGetTypeID() }
 }
 extension CFDictionary: CFType {
+    /// Returns the Core Foundation type identifier for a `CFDictionary` type.
     public static var typeID: CFTypeID { CFDictionaryGetTypeID() }
 }
 extension CFSet: CFType {
+    /// Returns the Core Foundation type identifier for a `CFSet` type.
     public static var typeID: CFTypeID { CFSetGetTypeID() }
 }
 extension CFDate: CFType {
+    /// Returns the Core Foundation type identifier for a `CFDate` type.
     public static var typeID: CFTypeID { CFDateGetTypeID() }
 }
 extension CFURL: CFType {
+    /// Returns the Core Foundation type identifier for a `CFURL` type.
     public static var typeID: CFTypeID { CFURLGetTypeID() }
 }
 extension CFBag: CFType {
+    /// Returns the Core Foundation type identifier for a `CFBag` type.
     public static var typeID: CFTypeID { CFBagGetTypeID() }
 }
 extension CFData: CFType {
+    /// Returns the Core Foundation type identifier for a `CFData` type.
     public static var typeID: CFTypeID { CFDataGetTypeID() }
 }
 
