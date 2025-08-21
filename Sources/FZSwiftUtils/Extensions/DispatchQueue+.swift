7b04ff77c2d90a9e3230f2133a602664441d3176
Florian Zand <florianzand@me.com>
2025-08-21 20:53:48 +0200

    Update DispatchQueue+.swift

--- Sources/FZSwiftUtils/Extensions/DispatchQueue+.swift
+++ Sources/FZSwiftUtils/Extensions/DispatchQueue+.swift
@@ -86,22 +86,22 @@ public extension DispatchQueue {
     
     /// The global system queue for maintenance or cleanup tasks that you create.
     static var background: DispatchQueue {
-        DispatchQueue.global(qos: .background)
+        .global(qos: .background)
     }
     
     /// The global system queue for tasks that the user does not track actively.
     static var utility: DispatchQueue {
-        DispatchQueue.global(qos: .utility)
+        .global(qos: .utility)
     }
     
     /// The global system queue for tasks that prevent the user from actively using your app.
     static var userInitiated: DispatchQueue {
-        DispatchQueue.global(qos: .userInitiated)
+        .global(qos: .userInitiated)
     }
     
     /// The global system queue for user-interactive tasks, such as animations, event handling, or updating your app’s user interface.
     static var userInteractive: DispatchQueue {
-        DispatchQueue.global(qos: .userInteractive)
+        .global(qos: .userInteractive)
     }
 }
 
@@ -116,14 +116,57 @@ public extension DispatchWallTime {
 
 extension DispatchQueue {
     /**
-     Executes the provided `DispatchWorkItem` synchronously on the queue, but avoids deadlock by performing
-     the work item directly if already on the queue.
+     Registers a queue so that it can later be detected as the current executing queue.
+
+     By default, the system queues (`.main`, and all global QoS queues) are automatically registered. Use this method to register additional custom queues for detection.
+
+     - Parameter queue: The dispatch queue to register.
+     */
+    public static func registerDetection(of queue: DispatchQueue) {
+        _registerDetection(of: [queue], key: key)
+    }
+    
+    /**
+     The dispatch queue currently executing.
+     
+     Returns `nil` if the current queue has not been registered.
+     
+     A dispatch queues needs to be registered first using ``registerDetection(of:)`` in order to be detectable. By default, the system queues (`.main`, and all global QoS queues) are automatically registered.
+     */
+    public static var current: DispatchQueue? { getSpecific(key: key)?.queue }
+    
+    /**
+     A Boolean value indicating whether the current code is executing on the specified dispatch queue.
+     
+     - Note: The dispatch queue must first be registered using ``registerDetection(of:)`` in order to be detectable.
+     */
+    public static func isExecuting(in queue: DispatchQueue) -> Bool {
+        DispatchQueue.current == queue
+    }
+    
+    private struct QueueReference { weak var queue: DispatchQueue? }
+    
+    private static let key: DispatchSpecificKey<QueueReference> = {
+        let key = DispatchSpecificKey<QueueReference>()
+        setupSystemQueuesDetection(key: key)
+        return key
+    }()
+    
+    private static func _registerDetection(of queues: [DispatchQueue], key: DispatchSpecificKey<QueueReference>) {
+        queues.forEach { $0.setSpecific(key: key, value: QueueReference(queue: $0)) }
+    }
+    
+    private static func setupSystemQueuesDetection(key: DispatchSpecificKey<QueueReference>) {
+        _registerDetection(of: [.main, .background, .global(qos: .default), .global(qos: .unspecified), .userInitiated, .userInteractive, .utility], key: key)
+    }
+    
+    /**
+     Executes the provided `DispatchWorkItem` synchronously on the queue, but avoids deadlock by performing the work item directly if already on the queue.
 
      - Parameter workItem: The `DispatchWorkItem` to execute.
      */
     public func syncSafely(execute workItem: DispatchWorkItem) {
-        setKeyIfNeeded()
-        if DispatchQueue.getSpecific(key: safeKey) != nil {
+        if DispatchQueue.isExecuting(in: self) {
             workItem.perform()
         } else {
             sync(execute: workItem)
@@ -131,14 +174,12 @@ extension DispatchQueue {
     }
     
     /**
-     Executes the provided closure synchronously on the queue, but avoids deadlock by executing the closure
-     directly if already on the queue.
+     Executes the provided closure synchronously on the queue, but avoids deadlock by executing the closure directly if already on the queue.
 
-     - Parameter block: The closure to execute.
+     - Parameter block: The block that contains the work to perform.
      */
     public func syncSafely(execute block: () -> Void) {
-        setKeyIfNeeded()
-        if DispatchQueue.getSpecific(key: safeKey) != nil {
+        if DispatchQueue.isExecuting(in: self) {
             block()
         } else {
             sync(execute: block)
@@ -146,16 +187,13 @@ extension DispatchQueue {
     }
     
     /**
-     Executes the provided throwing closure synchronously on the queue, avoiding deadlock by executing
-     it directly if already on the queue.
+     Executes the provided throwing closure synchronously on the queue, avoiding deadlock by executing it directly if already on the queue.
 
      - Parameter work: The closure to execute.
      - Returns: The result of the closure.
-     - Throws: Rethrows any error thrown by the closure.
      */
     public func syncSafely<T>(execute work: () throws -> T) rethrows -> T {
-        setKeyIfNeeded()
-        if DispatchQueue.getSpecific(key: safeKey) != nil {
+        if DispatchQueue.isExecuting(in: self) {
             return try work()
         } else {
             return try sync(execute: work)
@@ -163,39 +201,23 @@ extension DispatchQueue {
     }
     
     /**
-     Executes the provided throwing closure synchronously on the queue using the specified flags, avoiding
-     deadlock by executing it directly if already on the queue.
+     Executes the provided throwing closure synchronously on the queue using the specified flags, avoiding deadlock by executing it directly if already on the queue.
 
      - Parameters:
-       - flags: Flags to use for the sync call (e.g., `.barrier`, `.noQoS`).
-       - work: The closure to execute.
+        - flags: Additional attributes to apply when executing the block.
+        - work: The closure to execute.
      - Returns: The result of the closure.
-     - Throws: Rethrows any error thrown by the closure.
      */
     public func syncSafely<T>(flags: DispatchWorkItemFlags, execute work: () throws -> T) rethrows -> T {
-        setKeyIfNeeded()
-        if DispatchQueue.getSpecific(key: safeKey) != nil {
+        if DispatchQueue.isExecuting(in: self) {
             return try work()
         } else {
             return try sync(flags: flags, execute: work)
         }
     }
-    
-    private var didSetSafeKey: Bool {
-        get { getAssociatedValue("didSetSafeKey") ?? false }
-        set { setAssociatedValue(newValue, key: "didSetSafeKey") }
-    }
-    
-    private var safeKey: DispatchSpecificKey<Void> {
-        getAssociatedValue("safeKey", initialValue: DispatchSpecificKey<Void>())
-    }
-    
-    private func setKeyIfNeeded() {
-        guard !didSetSafeKey else { return }
-        setSpecific(key: safeKey, value: ())
-        didSetSafeKey = true
-    }
-    
+}
+
+extension DispatchQueue {
     /**
      Submits a single block to the dispatch queue and causes the block to be executed the specified number of times.
      
@@ -204,105 +226,151 @@ extension DispatchQueue {
      - Parameters:
         - iterations: The number of times to execute the block. Higher iteration values give the system the ability to balance more efficiently across multiple cores. To get the maximum benefit of this function, configure the number of iterations to be at least three times the number of available cores.
         - work: The block to execute in parallel. The block's iteration parameter specifies the current iteration index.
-        - progress: The block with the finished iterations count.
-        - completion: The block to execute when all iterations finished.
+        - progress: A handler that is called after each iteration.
+        - completion: A handler to execute when all iterations finished.
      */
     @_disfavoredOverload
     public class func concurrentPerform(iterations: Int, execute work: ((_ iteration: Int) -> Void), progress: ((_ finished: Int)->())? = nil, completion: (()->())? = nil) {
-        if progress != nil || completion != nil {
-            var completed = 0
-            let lock = DispatchQueue(label: "DispatchQueue.concurrentPerform.")
-            DispatchQueue.concurrentPerform(iterations: iterations) { index in
-                work(index)
-                lock.sync {
-                    completed += 1
-                    DispatchQueue.main.async {
-                        progress?(completed)
-                    }
-                    if completed == iterations {
-                        DispatchQueue.main.async {
-                            completion?()
-                        }
-                    }
+        guard progress != nil || completion != nil else {
+            DispatchQueue.concurrentPerform(iterations: iterations, execute: work)
+            return
+        }
+        let lock = NSLock()
+        var completed = 0
+        DispatchQueue.concurrentPerform(iterations: iterations) { index in
+            work(index)
+            lock.lock()
+            completed += 1
+            let count = completed
+            lock.unlock()
+            
+            if let progress = progress {
+                DispatchQueue.main.async {
+                    progress(count)
                 }
             }
-        } else {
-            DispatchQueue.concurrentPerform(iterations: iterations, execute: work)
+            
+            if count == iterations {
+                DispatchQueue.main.async {
+                    completion?()
+                }
+            }
         }
+    }
+    
+    /**
+     Executes the given closure concurrently for each element the specified collection.
+
+     The order of execution of the `work` closure is not guaranteed because operations are performed concurrently.
+     
+     The `progress` and `completion` closures are always called on the main thread.
+     
+     - Parameters:
+       - collection: The collection of elements to process concurrently.
+       - work: A closure that is executed for each element of the collection.
+       - progress: A handler that is called after each element completes.
+       - completion: A handler that is called after all elements have been processed.
+
+    Example usage:
+     
+     ```swift
+     let numbers = [1, 2, 3, 4, 5]
+     DispatchQueue.concurrentPerform(on: numbers, execute: { number in
+         print("Processing \(number)")
+     }, progress: { finished in
+         print("\(finished) items finished")
+     }, completion: {
+         print("All items processed")
+     })
+     ```
+     */
+    public class func concurrentPerform<C: RandomAccessCollection>(_ collection: C, execute work: ((_ element: C.Element) -> Void), progress: ((_ finished: Int)->())? = nil, completion: (()->())? = nil) {
+        concurrentPerform(iterations: collection.count, execute: { index in
+            work(collection[collection.index(collection.startIndex, offsetBy: index)])
+        }, progress: progress, completion: completion)
+    }
+    
+    /**
+     Executes the given closure concurrently for each element the specified sequence.
+
+     The order of execution of the `work` closure is not guaranteed because operations are performed concurrently.
+     
+     The `progress` and `completion` closures are always called on the main thread.
+     
+     - Parameters:
+       - sequence: The sequence of elements to process concurrently.
+       - work: A closure that is executed for each element of the collection.
+       - progress: A handler that is called after each element completes.
+       - completion: A handler that is called after all elements have been processed.
+
+    Example usage:
+     
+     ```swift
+     let numbers = [1, 2, 3, 4, 5]
+     DispatchQueue.concurrentPerform(on: numbers, execute: { number in
+         print("Processing \(number)")
+     }, progress: { finished in
+         print("\(finished) items finished")
+     }, completion: {
+         print("All items processed")
+     })
+     ```
+     */
+    public class func concurrentPerform<S: Sequence>(_ sequence: S, execute work: ((_ element: S.Element) -> Void), progress: ((_ finished: Int)->())? = nil, completion: (()->())? = nil) {
+        concurrentPerform(Array(sequence), execute: work, progress: progress, completion: completion)
     }
 }
 
-
 /*
- public struct Dispatch {
-     public static var main: Queue {
-         Queue(.main)
-     }
+extension DispatchQueue {
+    public func asyncSafely(execute workItem: DispatchWorkItem) {
+        if DispatchQueue.isExecuting(in: self) {
+            workItem.perform()
+        } else {
+            async(execute: workItem)
+        }
+    }
     
-     public static var background: Queue {
-         Queue(.global(qos: .background))
-     }
+    public func asyncSafely(execute block: @escaping () -> Void) {
+        if DispatchQueue.isExecuting(in: self) {
+            block()
+        } else {
+            async {
+                block()
+            }
+        }
+    }
     
-     public static var userInteractive: Queue {
-         Queue(.global(qos: .userInteractive))
-     }
+    /**
+     Executes a block immediately if already running on this queue, otherwise dispatches it asynchronously on this queue.
+
+     - Parameter work: The block to execute.
+     */
+    public func execOrAsync(execute work: @escaping @convention(block) () -> Void) {
+        if DispatchQueue.isExecuting(in: self) {
+            work()
+        } else {
+            async {
+                work()
+            }
+        }
+    }
     
-     public static var userInitiated: Queue {
-         Queue(.global(qos: .userInitiated))
-     }
-    
-     public static var utility: Queue {
-         Queue(.global(qos: .utility))
-     }
-    
-     public struct Queue {
-         let queue: DispatchQueue
-        
-         init(_ queue: DispatchQueue) {
-             self.queue = queue
-         }
-        
-         public func callAsFunction(execute: @escaping ()->()) {
-             queue.async(execute: execute)
-         }
-        
-         public func sync(execute: DispatchWorkItem) {
-             queue.sync(execute: execute)
-         }
-        
-         public func sync(execute block: () -> Void) {
-             queue.sync(execute: block)
-         }
-        
-         public func asyncAndWait(execute block: () -> Void) {
-             queue.asyncAndWait(execute: block)
-         }
-        
-         public func after(_ timeInterval: TimeInterval, execute: DispatchWorkItem) {
-             queue.asyncAfter(timeInterval, execute: execute)
-         }
-        
-         @_disfavoredOverload
-         public func after(_ timeDuration: TimeDuration, execute: DispatchWorkItem) {
-             queue.asyncAfter(timeDuration, execute: execute)
-         }
-        
-         public func after(_ timeInterval: TimeInterval, qos: DispatchQoS = .unspecified, flags: DispatchWorkItemFlags = [], execute: @escaping @Sendable ()->()) {
-             queue.asyncAfter(timeInterval, qos: qos, flags: flags, execute: execute)
-         }
-        
-         @_disfavoredOverload
-         public func after(_ timeDuration: TimeDuration, qos: DispatchQoS = .unspecified, flags: DispatchWorkItemFlags = [], execute: @escaping @Sendable ()->()) {
-             queue.asyncAfter(timeDuration, qos: qos, flags: flags, execute: execute)
-         }
-        
-         public func async(at date: Date, qos: DispatchQoS = .unspecified, flags: DispatchWorkItemFlags = [], execute work: @escaping @Sendable () -> Void) {
-             queue.async(at: date, qos: qos, flags: flags, execute: work)
-         }
-        
-         public func async(at date: Date,  execute: DispatchWorkItem) {
-             queue.async(at: date, execute: execute)
-         }
-     }
- }
- */
+    /**
+     Executes a block immediately if already running on this queue, otherwise dispatches it synchronously on this queue.
+
+     This method prevents deadlocks by skipping `sync` if the current queue is already the same.
+
+     - Parameter work: The block to execute.
+     */
+    public func execOrSync(execute work: @convention(block) () -> Void) {
+        if DispatchQueue.isExecuting(in: self) {
+            work()
+        } else {
+            sync {
+                work()
+            }
+        }
+    }
+}
+*/
