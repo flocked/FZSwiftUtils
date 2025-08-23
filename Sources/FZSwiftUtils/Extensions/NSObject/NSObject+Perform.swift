86aace99daee31f5e1807c0c34eaaebb71c8648f
Florian Zand <florianzand@me.com>
2025-08-23 02:08:43 +0200

    Update NSObject+Perform.swift

--- Sources/FZSwiftUtils/Extensions/NSObject/NSObject+Perform.swift
+++ Sources/FZSwiftUtils/Extensions/NSObject/NSObject+Perform.swift
@@ -10,41 +10,49 @@ import _NSObjectProxy
 
 extension NSObject {
     /**
-     Invokes the specified method of the object.
+     Sends a message to the receiver with the specified arguments.
      
      - Parameters:
-        - selector: The selector that identifies the method to invoke.
-        - arguments: The arguments to pass to the method when it is invoked.
+        - selector: The selector identifying the message to send.
+        - arguments: The arguments to pass to the message when it is invoked.
      */
     public func perform(_ selector: Selector, withArguments arguments: [Any?] = []) {
-        guard let signature = getMethodSignature(for: selector) else { return }
-        let invocation = Invocation(signature: signature)
-        invocation.target = self
-        invocation.selector = selector
-        invocation.arguments = arguments
-        invocation.invoke()
+        if arguments.isEmpty {
+            perform(selector)
+        } else if arguments.count == 1 {
+            perform(selector, with: arguments[0])
+        } else if arguments.count == 2 {
+            perform(selector, with: arguments[0], with: arguments[1])
+        } else {
+            guard let signature = getMethodSignature(for: selector) else { return }
+            let invocation = Invocation(signature: signature)
+            invocation.target = self
+            invocation.selector = selector
+            invocation.arguments = arguments
+            invocation.invoke()
+        }
     }
     
     /**
-     Invokes the specified method of the object.
+     Sends a message to the receiver with the specified arguments.
      
      - Parameters:
-        - selector: The selector that identifies the method to invoke.
+        - selector: The selector identifying the message to send.
         - arguments: The arguments to pass to the method when it is invoked.
      */
-    public func perform(_ selector: String, with arguments: [Any] = []) {
-        perform(NSSelectorFromString(selector), with: arguments)
+    public func perform(_ selector: String, withArguments arguments: [Any] = []) {
+        perform(NSSelectorFromString(selector), withArguments: arguments)
     }
     
     /**
-     Invokes the specified method of the object and returns its value.
+     Sends a message to the receiver with the specified arguments and returns the result of the message.
      
      - Parameters:
-        - selector: The selector that identifies the method to invoke.
+        - selector: The selector identifying the message to send.
         - arguments: The arguments to pass to the method when it is invoked.
-     - Returns: The return value of the method when it is invoked.
+     - Returns: The result of the message.
      */
-    public func perform<V>(_ selector: Selector, with arguments: [Any?] = []) -> V? {
+    public func perform<V>(_ selector: Selector, withArguments arguments: [Any?] = []) -> V? {
         guard let signature = getMethodSignature(for: selector) else { return nil }
         let invocation = Invocation(signature: signature)
         invocation.target = self
@@ -55,26 +63,27 @@ extension NSObject {
     }
     
     /**
-     Invokes the specified method of the object and returns its value.
-     
+     Sends a message to the receiver with the specified arguments and returns the result of the message.
+
      - Parameters:
-        - selector: The selector that identifies the method to invoke.
+        - selector: The selector identifying the message to send.
         - arguments: The arguments to pass to the method when it is invoked.
-     - Returns: The return value of the method when it is invoked.
+     - Returns: The result of the message.
      */
-    public func perform<V>(_ selector: String, with arguments: [Any?] = []) -> V? {
-        perform(NSSelectorFromString(selector), with: arguments)
+    public func perform<V>(_ selector: String, withArguments arguments: [Any?] = []) -> V? {
+        perform(NSSelectorFromString(selector), withArguments: arguments)
     }
     
     /**
-     Invokes the specified method of the object and returns its value.
+     Sends a message to the receiver with the specified arguments and returns the result of the message.
 
      - Parameters:
-        - selector: The selector that identifies the method to invoke.
+        - selector: The selector identifying the message to send.
         - arguments: The arguments to pass to the method when it is invoked.
-     - Returns: The return value of the method when it is invoked.
+     - Returns: The result of the message.
      */
-    public func performAndReturn(_ selector: Selector, with arguments: [Any?] = []) -> Any? {
+    @_disfavoredOverload
+    public func perform(_ selector: Selector, withArguments arguments: [Any?] = []) -> Any? {
         guard let signature = getMethodSignature(for: selector) else { return nil }
         let invocation = Invocation(signature: signature)
         invocation.target = self
@@ -85,14 +94,15 @@ extension NSObject {
     }
     
     /**
-     Invokes the specified method of the object and returns its value.
+     Sends a message to the receiver with the specified arguments and returns the result of the message.
 
      - Parameters:
-        - selector: The selector that identifies the method to invoke.
+        - selector: The selector identifying the message to send.
         - arguments: The arguments to pass to the method when it is invoked.
-     - Returns: The return value of the method when it is invoked.
+     - Returns: The result of the message.
      */
-    public func performAndReturn(_ selector: String, with arguments: [Any?] = []) -> Any? {
-        performAndReturn(NSSelectorFromString(selector), with: arguments)
+    @_disfavoredOverload
+    public func perform(_ selector: String, withArguments arguments: [Any?] = []) -> Any? {
+        perform(NSSelectorFromString(selector), withArguments: arguments)
     }
 }
