e87a611f6e37b01ba6ce42214fb00df38b12bab9
Florian Zand <florianzand@me.com>
2025-10-31 11:01:14 +0100

    Update Hasher+.swift

--- Sources/FZSwiftUtils/Extensions/Hash/Hasher+.swift
+++ Sources/FZSwiftUtils/Extensions/Hash/Hasher+.swift
@@ -19,6 +19,24 @@ extension Hasher {
      - Parameter elements: A sequence of elements that conform to `Hashable`.
      - Returns: The hash value for the `elements`.
      */
+    public static func hash(_ elements: [any Hashable]) -> Int {
+        var hasher = Hasher()
+        elements.forEach { hasher.combine($0) }
+        return hasher.finalize()
+    }
+    
+    /**
+     Computes a hash value for the specified `Hashable` elements.
+     
+     Example usage:
+     ```swift
+     let hashValue = Hasher.hash([1, "String", 3.7])
+     ```
+
+     - Parameter elements: A sequence of elements that conform to `Hashable`.
+     - Returns: The hash value for the `elements`.
+     */
+    @_disfavoredOverload
     public static func hash<S: Sequence>(_ elements: S) -> Int where S.Element == any Hashable {
         var hasher = Hasher()
         elements.forEach { hasher.combine($0) }
