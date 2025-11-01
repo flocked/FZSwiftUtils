6886efeac623a468a7541c3419753bea10de1bfa
Florian Zand <florianzand@me.com>
2025-11-01 21:48:55 +0100

    updated

--- Sources/FZSwiftUtils/Utils/ImageSource/ImageDestination.swift
+++ Sources/FZSwiftUtils/Utils/ImageSource/ImageDestination.swift
@@ -55,11 +55,12 @@ public class ImageDestination {
      
      - Parameters:
         - image: The image to add.
+        - options: Options how to add the image.
         - properties: An optional dictionary that specifies the properties of the added image.
      */
-    public func addImage(_ image: CGImage, properties: [CFString: Any]? = nil) {
+    public func addImage(_ image: CGImage, options: ImageOptions = .init(), properties: [CFString: Any]? = nil) {
         guard !isFinalized else { return }
-        steps += .image(image, metadata: nil, properties: properties)
+        steps += .image(image, metadata: nil, properties: options.dictionary(with: properties))
         imageCount += 1
     }
     
@@ -69,11 +70,12 @@ public class ImageDestination {
      - Parameters:
         - image: The image to add.
         - metadata: The metadata for the image to add.
+        - options: Options how to add the image.
         - properties: An optional dictionary that specifies the properties of the added image.
      */
-    public func addImage(_ image: CGImage, metadata: CGImageMetadata, properties: [CFString: Any]? = nil) {
+    public func addImage(_ image: CGImage, metadata: CGImageMetadata, options: ImageOptions = .init(), properties: [CFString: Any]? = nil) {
         guard !isFinalized else { return }
-        steps += .image(image, metadata: metadata, properties: properties)
+        steps += .image(image, metadata: metadata, properties: options.dictionary(with: properties))
         imageCount += 1
     }
     
@@ -83,11 +85,12 @@ public class ImageDestination {
      - Parameters:
         - source: An image source that contains the image.
         - index: The index of the image in the image source.
+        - options: Options how to add the image.
         - properties: An optional dictionary that specifies additional image property information. The added image automatically inherits the properties found in the image source. Use this dictionary to add properties to the image, or to modify one of the inherited properties. To remove an inherited property altogether, specify NULL for the property’s value.
      */
-    public func addImage(from source: CGImageSource, at index: Int) {
+    public func addImage(from source: CGImageSource, at index: Int, options: ImageOptions = .init()) {
         guard !isFinalized else { return }
-        steps += .sourceImage(imageSource: source, index: index, properties: nil)
+        steps += .sourceImage(imageSource: source, index: index, properties: options.dictionary)
         imageCount += 1
     }
     
@@ -182,7 +185,87 @@ public class ImageDestination {
     public static var supportedContentTypes: [UTType] {
         supportedTypeIdentifers.compactMap({  UTType($0) })
     }
-    
+}
+
+extension ImageDestination {
+    /// The options for adding an an image to the destination.
+    public struct ImageOptions {
+        
+        /**
+         The maximum width and height of the image.
+         
+         If present, the destination rescales the image as needed to fit within the maximum width and height. If this key isn’t present, the destination retains the native image size.
+         */
+        public var maxSize: Int?
+        
+        /**
+         The desired compression quality to use when writing the image data.
+         
+         The compression factor must be in the range of `0.0` to `1.0`. A value of `1.0` specifies to use lossless compression if destination format supports it. A value of `0.0` implies to use maximum compression.
+         */
+        public var compressionQuality: CGFloat? {
+            didSet { compressionQuality?.clamp(to: 0...1) }
+        }
+        
+        /**
+         ckground color to use when the image has an alpha component, but the destination format doesn’t support alpha.
+         
+         The color you provide must be a CGColor without an alpha component of its own. If the property is `nil` and a background color is needed, a white color is used.
+         */
+        public var backgroundColor: CGColor?
+        
+        /// A Boolean value that indicates whether to embed a thumbnail for JPEG and HEIF images.
+        public var embedThumbnail = false
+        
+        /**
+         A Boolean value that indicates whether to create the image using a colorspace.
+         
+         If you set this property to `true`, the image is color converted using its colorspace, which provides better compatibility with older devices.
+         */
+        public var optimizeColorForSharing = false
+        
+        /**
+         A Boolean value that indicates whether to include a HEIF-embedded gain map in the image data.
+         
+         If you scale the destination image using the ``maxSize`` property, the destination also scales the gain map.
+         */
+        @available(macOS 11.0, iOS 14.1, tvOS 14.1, watchOS 7.0, *)
+        public var preserveGainMap: Bool {
+            get { _preserveGainMap }
+            set { _preserveGainMap = newValue }
+        }
+        var _preserveGainMap = false
+        
+        public init(maxSize: Int? = nil, compressionQuality: CGFloat? = nil, backgroundColor: CGColor? = nil, embedThumbnail: Bool = false, optimizeColorForSharing: Bool = false) {
+            self.maxSize = maxSize
+            self.compressionQuality = compressionQuality
+            self.backgroundColor = backgroundColor
+            self.embedThumbnail = embedThumbnail
+            self.optimizeColorForSharing = optimizeColorForSharing
+        }
+        
+        func dictionary(with properties: [CFString:Any]?) -> [CFString:Any]? {
+            guard var properties = properties else { return dictionary }
+            dictionary.forEach({ properties[$0.key] = $0.value })
+            return properties.isEmpty ? nil : properties
+        }
+        
+        var dictionary: [CFString: Any] {
+            var dict: [CFString: Any] = [:]
+            dict[kCGImageDestinationImageMaxPixelSize] = maxSize
+            dict[kCGImageDestinationBackgroundColor] = backgroundColor
+            dict[kCGImageDestinationLossyCompressionQuality] = compressionQuality
+            if embedThumbnail { dict[kCGImageDestinationEmbedThumbnail] = true }
+            if optimizeColorForSharing { dict[kCGImageDestinationOptimizeColorForSharing] = true }
+            if #available(macOS 11.0, iOS 14.1, tvOS 14.1, watchOS 7.0, *) {
+                if preserveGainMap { dict[kCGImageDestinationPreserveGainMap] = true }
+            }
+            return dict
+        }
+    }
+}
+
+extension ImageDestination {
     private enum Errors: LocalizedError {
         case finalizationFailed
         case alreadyFinalized
