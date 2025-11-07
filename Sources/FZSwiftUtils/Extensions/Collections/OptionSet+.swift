d0ec2042488075e64b5c0873069e2d68e0c48199
Florian Zand <florianzand@me.com>
2025-11-07 18:30:20 +0100

    updated

--- Sources/FZSwiftUtils/Extensions/Collections/OptionSet+.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/OptionSet+.swift
@@ -84,3 +84,31 @@ public extension OptionSet where RawValue: FixedWidthInteger, Element == Self {
         }
     }
 }
+
+extension OptionSet where RawValue: FixedWidthInteger {
+    /**
+     Creates a new option set from the given bit index.
+          
+     - Parameter bitIndex: The zero-based index of the bit to set. Must be less than `RawValue.bitWidth`.
+          
+     Example:
+     ```swift
+     struct Direction: OptionSet {
+        let rawValue: Int
+
+        static let left = Direction(bitIndex: 0)
+        static let right = Direction(bitIndex: 1)
+        static let down = Direction(bitIndex: 2)
+        static let up = Direction(bitIndex: 3)
+     
+        init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+     }
+     ```
+     */
+    public init(bitIndex: UInt) {
+        precondition(bitIndex < RawValue.bitWidth, "Bit index out of range.")
+        self.init(rawValue: 1 << Int(bitIndex))
+    }
+}
