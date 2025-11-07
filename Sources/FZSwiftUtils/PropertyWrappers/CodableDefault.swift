a60429838edf0956879c0dbb42a4af845c1d767b
Florian Zand <florianzand@me.com>
2025-11-07 18:52:15 +0100

    Update CodableDefault.swift

--- Sources/FZSwiftUtils/PropertyWrappers/CodableDefault.swift
+++ Sources/FZSwiftUtils/PropertyWrappers/CodableDefault.swift
@@ -91,3 +91,23 @@ public extension KeyedDecodingContainer {
         }
     }
 }
+
+public extension KeyedDecodingContainer {
+    /**
+     Decodes a value of the given type for the given key.
+     
+     - Parameter key: The key that the decoded value is associated with.
+     */
+    func decode<T: Decodable>(_ key: Key) throws -> T {
+        try decode(T.self, forKey: key)
+    }
+    
+    /**
+     Decodes a value of the given type for the given key, if present.
+     
+     - Parameter key: The key that the decoded value is associated with.
+     */
+    func decodeIfPresent<T: Decodable>(_ key: Key) throws -> T? {
+        try decodeIfPresent(T.self, forKey: key)
+    }
+}
