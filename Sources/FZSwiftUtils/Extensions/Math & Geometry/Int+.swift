5a9e30d625e0d50ffaff76fc6acfa500d4906c42
Florian Zand <florianzand@me.com>
2025-11-07 17:58:23 +0100

    Update Int+.swift

--- Sources/FZSwiftUtils/Extensions/Math & Geometry/Int+.swift
+++ Sources/FZSwiftUtils/Extensions/Math & Geometry/Int+.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension BinaryInteger {
     /// Returns the shift value for which `1 << shiftValue` equals the integer, or `nil` if not a power of two.
-    var shiftValue: Int? {
+    public var shiftValue: Int? {
         guard self > 0 else { return nil }
         return Int(log2(Double(self)))
     }
