abcd4c511cab3909fb3872bcebc45fed3f3fe889
Florian Zand <florianzand@me.com>
2025-11-15 11:38:18 +0100

    updated

--- Sources/FZSwiftUtils/Extensions/Protocol/Decodable+.swift
+++ Sources/FZSwiftUtils/Extensions/Protocol/Decodable+.swift
@@ -7,7 +7,34 @@
 
 import Foundation
 
-struct _EmptyDecodable: Decodable {}
+public extension SingleValueDecodingContainer {
+    /// Decodes a single value of the given type.
+    func decode<T: Decodable>() throws -> T {
+        try decode(T.self)
+    }
+}
+
+public extension KeyedDecodingContainer {
+    /**
+     Decodes a value of the given type for the given key.
+     
+     - Parameter key: The key that the decoded value is associated with.
+     */
+    func decode<T: Decodable>(_ key: Key) throws -> T {
+        try decode(T.self, forKey: key)
+    }
+    
+    /**
+     Decodes a value of the given type for the given key, if present.
+     
+     - Parameter key: The key that the decoded value is associated with.
+     */
+    func decodeIfPresent<T: Decodable>(_ key: Key) throws -> T? {
+        try decodeIfPresent(T.self, forKey: key)
+    }
+}
+
+struct _EmptyDecodable: Decodable { }
 
 public extension UnkeyedDecodingContainer {
     /// Skips decoding.
