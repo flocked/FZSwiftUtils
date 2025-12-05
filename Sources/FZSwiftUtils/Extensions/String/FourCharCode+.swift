9b100fe85a8595016164042f0a67903704bd3080
Florian Zand <florianzand@me.com>
2025-12-05 19:09:05 +0100

    Create FourCharCode+.swift

---
+++ Sources/FZSwiftUtils/Extensions/String/FourCharCode+.swift
@@ -0,0 +1,44 @@
+//
+//  FourCharCode+.swift
+//
+//
+//  Created by Florian Zand on 05.12.25.
+//
+
+import Foundation
+
+extension FourCharCode: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) {
+        var code: FourCharCode = 0
+        if value.count == 4 && value.utf8.count == 4 {
+            for byte in value.utf8 {
+                code = (code << 8) | FourCharCode(byte)
+            }
+        } else {
+            print("FourCharCode: Can't initialize with '\(value)', only printable ASCII allowed. Setting to '????'.")
+            code = 0x3F3F3F3F // = '????'
+        }
+        self = code
+    }
+    
+    public init(extendedGraphemeClusterLiteral value: String) {
+        self = FourCharCode(stringLiteral: value)
+    }
+    
+    public init(unicodeScalarLiteral value: String) {
+        self = FourCharCode(stringLiteral: value)
+    }
+    
+    public init(_ value: String) {
+        self = FourCharCode(stringLiteral: value)
+    }
+
+    public var string: String {
+        let n = Int(self)
+        var s = String(UnicodeScalar((n >> 24) & 255)!)
+        s += String(UnicodeScalar((n >> 16) & 255)!)
+        s += String(UnicodeScalar((n >> 8) & 255)!)
+        s += String(UnicodeScalar(n & 255)!)
+        return s.trimmingCharacters(in: .whitespaces)
+    }
+}
