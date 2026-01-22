963d8dc07dca627eb058373e724a87536bb6fdb2
Florian Zand <florianzand@me.com>
2026-01-23 00:41:45 +0100

    updated

--- Sources/FZSwiftUtils/Extensions/Encoding & Decoding/NSCoding+.swift
+++ Sources/FZSwiftUtils/Extensions/Encoding & Decoding/NSCoding+.swift
@@ -10,15 +10,12 @@ import Foundation
 public extension NSCoding {
     /**
      Archives the object into `Data`.
-     
-     If the object conforms to `NSSecureCoding`, set `requiringSecureCoding` to `true` for added security.
-     
-     - Parameter requiringSecureCoding: A Boolean value indicating whether secure coding is required.
+          
      - Throws: An error if the encoding process fails.
      - Returns: A `Data` representation of the object.
      */
-    func archivedData(requiringSecureCoding: Bool = false) throws -> Data {
-        try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: requiringSecureCoding)
+    func archivedData() throws -> Data {
+        try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: false)
     }
 
     /**
@@ -40,6 +37,62 @@ public extension NSCoding {
     func archiveBasedCopy<Subclass: NSCoding>(as subclass: Subclass.Type) throws -> Subclass {
         try NSKeyedUnarchiver.unarchivedObject(from: try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: false, as: subclass))
     }
+    
+    /**
+     Decodes a previously-archived object graph, and returns the root object as the type.
+     
+     - Parameter data: The object graph previously encoded by `NSKeyedArchiver`.
+     */
+    static func unarchive(_ data: Data) throws -> Self {
+        guard let object = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? Self else { throw CocoaError(.coderReadCorrupt) }
+        return object
+      // try NSKeyedUnarchiver.unarchivedObject(from: data)
+    }
+}
+
+public extension NSSecureCoding {
+    /**
+     Archives the object into `Data`.
+          
+     - Throws: An error if the encoding process fails.
+     - Returns: A `Data` representation of the object.
+     */
+    func archivedData() throws -> Data {
+        try archivedData(requiresSecureCoding: Self.supportsSecureCoding)
+    }
+    
+    /**
+     Archives the object into `Data`.
+     
+     - Parameter requiresSecureCoding: A Boolean value indicating whether the unarchived object requires to conform to [NSSecureCoding](https://developer.apple.com/documentation/foundation/nssecurecoding).
+     - Throws: An error if the encoding process fails.
+     - Returns: A `Data` representation of the object.
+     */
+    func archivedData(requiresSecureCoding: Bool) throws -> Data {
+        try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: requiresSecureCoding)
+    }
+    
+    /*
+    /**
+     Decodes a previously-archived object graph, and returns the root object as the type.
+     
+     - Parameter data: The object graph previously encoded by `NSKeyedArchiver`.
+     */
+    static func unarchive(_ data: Data) throws -> Self {
+       try unarchive(data, requiresSecureCoding: supportsSecureCoding)
+    }
+    
+    /**
+     Decodes a previously-archived object graph, and returns the root object as the type.
+     
+     - Parameters:
+        - data: The object graph previously encoded by `NSKeyedArchiver`.
+        - requiresSecureCoding: A Boolean value indicating whether the unarchived object requires to conform to [NSSecureCoding](https://developer.apple.com/documentation/foundation/nssecurecoding).
+     */
+    static func unarchive(_ data: Data, requiresSecureCoding: Bool) throws -> Self {
+       try NSKeyedUnarchiver.unarchivedObject(from: data, requiresSecureCoding: requiresSecureCoding)
+    }
+     */
 }
 
 public extension NSCoding where Self: NSObject {
