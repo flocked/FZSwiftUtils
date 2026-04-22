26c71788204b2d2874e0b6c428532f45bc4299c5
Florian Zand <florianzand@me.com>
2026-04-22 18:42:38 +0200

    Update Dictionary+.swift

--- Sources/FZSwiftUtils/Extensions/Collections/Dictionary+.swift
+++ Sources/FZSwiftUtils/Extensions/Collections/Dictionary+.swift
@@ -344,17 +344,17 @@ public extension Dictionary where Value: Equatable {
 
 public extension Dictionary where Value == Any {
     /// Returns the value casted to the requested type, or `nil` if the value is missing or of a different type.
-    subscript<T>(key: Key) -> T? {
+    subscript<T>(typed key: Key) -> T? {
         self[key] as? T
     }
 
     /// Returns the value casted to the requested type, or the specified default value if missing or of a different type.
-    subscript<T>(key: Key, default defaultValue: @autoclosure () -> T) -> T {
+    subscript<T>(typed key: Key, default defaultValue: @autoclosure () -> T) -> T {
         (self[key] as? T) ?? defaultValue()
     }
     
     /// Returns the value casted to the requested type, or the specified default value if missing or of a different type.
-    subscript<T>(key: Key, default defaultValue: () -> T) -> T {
+    subscript<T>(typed key: Key, default defaultValue: () -> T) -> T {
         (self[key] as? T) ?? defaultValue()
     }
     
