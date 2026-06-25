c2ea940483fe5874c33865bb46e6c2c91aa3c067
Florian Zand <florianzand@me.com>
2026-06-25 11:20:00 +0200

    Update NSCoder+.swift

--- Sources/FZSwiftUtils/Extensions/NSCoder+.swift
+++ Sources/FZSwiftUtils/Extensions/NSCoder+.swift
@@ -22,7 +22,6 @@ public extension NSCoder {
     /// Encodes the specified value for the given key.
     func encode<V: _ObjectiveCBridgeable>(_ value: V?, forKey key: String) {
         guard let value = value else { return }
-        
         encode(value._bridgeToObjectiveC(), forKey: key)
     }
     
