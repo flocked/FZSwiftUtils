b85d9554fea4f15e71daff147985683b47c208fb
Florian Zand <florianzand@me.com>
2026-07-25 06:39:26 +0200

    Update URLSessionTask+.swift

--- Sources/FZSwiftUtils/Extensions/Networking/URLSessionTask+.swift
+++ Sources/FZSwiftUtils/Extensions/Networking/URLSessionTask+.swift
@@ -34,4 +34,53 @@ public extension URLSessionTask {
         guard let bytes = expectedContentLength else { return nil }
         return .bytes(bytes)
     }
+    
+    /// Sets the relative priority at which you’d like a host to handle the task, specified as a floating point value between 0.0 (lowest priority) and 1.0 (highest priority).
+    @discardableResult
+    func priority(_ priority: Float) -> Self {
+        self.priority = priority
+        return self
+    }
+
+    /// Sets the app-provided string value for the current task.
+    @discardableResult
+    func taskDescription(_ description: String?) -> Self {
+        self.taskDescription = description
+        return self
+    }
+
+    /// Sets the earliest date at which the network load should begin.
+    @discardableResult
+    func earliestBeginDate(_ date: Date?) -> Self {
+        self.earliestBeginDate = date
+        return self
+    }
+
+    /// Sets the delegate specific to the task.
+    @discardableResult
+    func delegate(_ delegate: (any URLSessionTaskDelegate)?) -> Self {
+        self.delegate = delegate
+        return self
+    }
+
+    /// Sets the best-guess upper bound on the number of bytes the client expects to receive.
+    @discardableResult
+    func countOfBytesClientExpectsToReceive(_ count: Int64) -> Self {
+        self.countOfBytesClientExpectsToReceive = count
+        return self
+    }
+
+    /// Sets the best-guess upper bound on the number of bytes the client expects to send.
+    @discardableResult
+    func countOfBytesClientExpectsToSend(_ count: Int64) -> Self {
+        self.countOfBytesClientExpectsToSend = count
+        return self
+    }
+
+    /// Sets the Boolean value indicating whether the task prefers incremental deleviery.
+    @discardableResult
+    func prefersIncrementalDelivery(_ prefers: Bool) -> Self {
+        self.prefersIncrementalDelivery = prefers
+        return self
+    }
 }
